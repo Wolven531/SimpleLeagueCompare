4fdcb6ad498c41fc0c2284531b1b2f6b62ed29db
Anthony Williams <anthony.williams.cs@gmail.com>
2020-04-21 20:18:43 -0600

    Update test text

--- simpleleaguecompare/src/App.test.tsx
+++ simpleleaguecompare/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { App } from './App'
 
-test('renders learn react link', () => {
+test('renders league champs link', () => {
   const { getByText } = render(<App />)
   const linkElement = getByText(/Champions:/i)
 
