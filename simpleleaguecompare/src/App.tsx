03d19efff76551ef5df2f5e1937aa657e68a1d98
Anthony Williams <anthony.williams.cs@gmail.com>
2020-04-21 20:17:41 -0600

    Update source w/ champs link

--- simpleleaguecompare/src/App.tsx
+++ simpleleaguecompare/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const API_V = '10.8.1'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <ul>
+        <li>
+          Champions:&nbsp;
+          <a
+            href={`//ddragon.leagueoflegends.com/cdn/${API_V}/data/en_US/champion.json`}
+            rel="noopener noreferrer"
+            target="_blank"
+            >//ddragon.leagueoflegends.com/cdn/{API_V}/data/en_US/champion.json</a></li>
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export { App }
