787de34148eab0e1b8458fc1adc20121829933d6
Anthony Williams <anthony.williams.cs@gmail.com>
2020-04-21 20:23:33 -0600

    Add spin mode button

--- simpleleaguecompare/src/App.tsx
+++ simpleleaguecompare/src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
   const API_V = '10.8.1'
 
+  let [isSpinning, setIsSpinning] = useState(false)
+
   return (
     <div className="app">
       <ul>
-        <li>
+        <li className={ isSpinning ? 'spinning' : '' }>
           Champions:&nbsp;
           <a
             href={`//ddragon.leagueoflegends.com/cdn/${API_V}/data/en_US/champion.json`}
@@ -15,6 +17,9 @@ const App = () => {
             target="_blank"
             >//ddragon.leagueoflegends.com/cdn/{API_V}/data/en_US/champion.json</a></li>
       </ul>
+      <button onClick={() => { setIsSpinning(staleSpinning => !staleSpinning) }}>
+        Toggle Spin Mode!
+      </button>
     </div>
   )
 }
