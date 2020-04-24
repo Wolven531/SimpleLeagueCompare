ce3d4e458a4fa0e91db57a141d63e52f04791c8f
Anthony Williams <anthony.williams.cs@gmail.com>
2020-04-24 17:58:27 -0600

    Add non-functional links to matchlists

--- simpleleaguecompare/src/App/App.tsx
+++ simpleleaguecompare/src/App/App.tsx
@@ -6,7 +6,11 @@ import './App.css'
 // }
 
 const App = () => {
+	const ACCT_ENCRYPTED_ANTHONY = 'U9b-KVWyJkTpQR0YiPJt7U8DFqy5llDfTJZYV56-G7onXevEOMC_DiI1'
+	const ACCT_ENCRYPTED_NICOLE = 'Cn0MOwyHpDXOLaCqpbkwMoIs1M8r9IJnv39DOM867E1zTjE'
+	const ACCT_ENCRYPTED_VINNY = 'aME0ZGruQhV8etyYYIys4vqFarj13QyvFztnwVIHImEgEgiwl7OLPsRE'
 	const API_V = '10.8.1'
+	const REGION = 'na1'
 
 	let [devAPIKey, setDevAPIKey] = useState('')
 	let [isSpinning, setIsSpinning] = useState(false)
@@ -71,6 +75,27 @@ const App = () => {
 					>
 						//ddragon.leagueoflegends.com/cdn/{API_V}/data/en_US/champion.json
 					</a>
+				</li>
+				<li>
+					Match list for Anthony:&nbsp;
+					<a
+						href={`//${REGION}.api.riotgames.com/lol/match/v4/matchlists/by-account/${ACCT_ENCRYPTED_ANTHONY}`}
+						rel="noopener noreferrer"
+						target="_blank">Anthony's Matchlist</a>
+				</li>
+				<li>
+					Match list for Nicole:&nbsp;
+					<a
+						href={`//${REGION}.api.riotgames.com/lol/match/v4/matchlists/by-account/${ACCT_ENCRYPTED_NICOLE}`}
+						rel="noopener noreferrer"
+						target="_blank">Nicole's Matchlist</a>
+				</li>
+				<li>
+					Match list for Vinny:&nbsp;
+					<a
+						href={`//${REGION}.api.riotgames.com/lol/match/v4/matchlists/by-account/${ACCT_ENCRYPTED_VINNY}`}
+						rel="noopener noreferrer"
+						target="_blank">Vinny's Matchlist</a>
 				</li>
 			</ul>
 			<button onClick={toggleSpinMode}>Toggle Spin Mode!</button>
