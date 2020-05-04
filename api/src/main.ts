5c9afe7886a7edc0a1594912fb788226b2bc71db
Anthony Williams <anthony.williams.cs@gmail.com>
2020-05-03 20:57:47 -0600

    Load NestJS API_PORT from env

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,15 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  const port = 3049
+  const configService = app.get(ConfigService)
+  const port = configService.get('API_PORT', 3000)
+
   console.log(`Starting to listen for NestJS app on port ${port}...`)
 
-  // TODO: load this from process.env
   await app.listen(port)
 }
 
