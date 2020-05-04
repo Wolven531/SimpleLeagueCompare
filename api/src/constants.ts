833a225e682a37e6629b7fbbdc961ccec31f81fa
Anthony Williams <anthony.williams.cs@gmail.com>
2020-05-03 21:00:02 -0600

    Introduce + use constants file in API

---
+++ api/src/constants.ts
@@ -0,0 +1,2 @@
+export const ENV_API_KEY = 'API_PORT'
+export const ENV_DEFAULT_API_PORT = 3000
--- api/src/main.ts
+++ api/src/main.ts
@@ -2,11 +2,12 @@ import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
+import { ENV_API_KEY, ENV_DEFAULT_API_PORT } from './constants'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
-  const port = configService.get('API_PORT', 3000)
+  const port = configService.get(ENV_API_KEY, ENV_DEFAULT_API_PORT)
 
   console.log(`Starting to listen for NestJS app on port ${port}...`)
 
