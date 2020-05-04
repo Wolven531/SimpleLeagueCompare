63ff05c42d216b3ec99defd01f1d416b85b2b112
Anthony Williams <anthony.williams.cs@gmail.com>
2020-05-03 20:52:37 -0600

    Light source reformat, log out API port

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
+  const port = 3049
+  console.log(`Starting to listen for NestJS app on port ${port}...`)
+
   // TODO: load this from process.env
-  await app.listen(3049);
+  await app.listen(port)
 }
-bootstrap();
+
+bootstrap()
