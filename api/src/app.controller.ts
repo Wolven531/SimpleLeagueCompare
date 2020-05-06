d0c3d654a6e669e132a33658cd5997af3cfa149c
Anthony Williams <anthony.williams.cs@gmail.com>
2020-05-05 18:43:57 -0600

    Add note about accountId param

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -12,6 +12,8 @@ export class AppController {
 
   @Get('matchlist')
   async getMatchlist(): Promise<any[]> {
+    // TODO: parameterize accountId
+    // see: https://docs.nestjs.com/controllers#route-parameters
     const accountId = 'U9b-KVWyJkTpQR0YiPJt7U8DFqy5llDfTJZYV56-G7onXevEOMC_DiI1'
 
     return this.appService.getMatchlist(accountId, accountId)
