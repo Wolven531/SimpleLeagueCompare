10a6ff9a862e090ff467445ab0ef1faa9a3bf0a1
Anthony Williams <anthony.williams.cs@gmail.com>
2020-05-05 20:38:58 -0600

    Move CORS enablement into creation line

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,10 +5,7 @@ import { AppModule } from './app.module'
 import { ENV_API_KEY, ENV_DEFAULT_API_PORT } from './constants'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  console.log(`[ bootstrap | main ] Enabling CORS...`)
-  app.enableCors()
-
+  const app = await NestFactory.create(AppModule, { cors: true })
   const configService = app.get(ConfigService)
   const port = configService.get(ENV_API_KEY, ENV_DEFAULT_API_PORT)
 
