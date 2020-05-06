cff020dbff1287f9744114214d22d732502a42af
Anthony Williams <anthony.williams.cs@gmail.com>
2020-05-05 20:09:05 -0600

    Pass API key into service

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -15,7 +15,8 @@ export class AppController {
     // TODO: parameterize accountId
     // see: https://docs.nestjs.com/controllers#route-parameters
     const accountId = 'U9b-KVWyJkTpQR0YiPJt7U8DFqy5llDfTJZYV56-G7onXevEOMC_DiI1'
+    const apiKey = 'RGAPI-5de0a5bb-7903-4d74-b1ac-dbef09e7f2a9'
 
-    return this.appService.getMatchlist(accountId, accountId)
+    return this.appService.getMatchlist(apiKey, accountId)
   }
 }
