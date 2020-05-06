affcf4acbeb6e7c54629b77cfcdccb4846803faa
Anthony Williams <anthony.williams.cs@gmail.com>
2020-05-05 20:25:51 -0600

    Enable CORS for th API (NestJS app)

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,10 +6,13 @@ import { ENV_API_KEY, ENV_DEFAULT_API_PORT } from './constants'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  console.log(`[ bootstrap | main ] Enabling CORS...`)
+  app.enableCors()
+
   const configService = app.get(ConfigService)
   const port = configService.get(ENV_API_KEY, ENV_DEFAULT_API_PORT)
 
-  console.log(`Starting to listen for NestJS app on port ${port}...`)
+  console.log(`[ bootstrap | main ] Starting to listen for NestJS app on port ${port}...`)
 
   await app.listen(port)
 }
