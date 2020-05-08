0718888a1be6abb093fcf3ac79ba0aec7a2688c7
Anthony Williams <anthony.williams.cs@gmail.com>
2020-05-07 19:26:02 -0600

    Safely extract fetchChamps()

--- simpleleaguecompare/src/components/APIConfigInfo.tsx
+++ simpleleaguecompare/src/components/APIConfigInfo.tsx
@@ -6,16 +6,40 @@ export interface IAPIConfigInfoProps {
 	onAPIKeySaved: (newKey: string) => void
 }
 
-const APIConfigInfo: FC<IAPIConfigInfoProps> = ({ onAPIKeySaved }) => {
+const fetchChamps = async (): Promise<any> => {
 	const API_V = '10.8.1'
-	const KEY_API_KEY = 'simpleLeagueCompare.API-dev'
 	const KEY_CHAMPS = 'simpleLeagueCompare.champs'
+	const KEY_CHAMPS_LAST_SAVED = 'simpleLeagueCompare.saved.champs'
+	const genTimestamp = (): string => String((new Date()).getTime())
+
+	return fetch(`https://ddragon.leagueoflegends.com/cdn/${API_V}/data/en_US/champion.json`)
+		.then(resp => resp.json())
+		.then(({ data }) => {
+			const champMap: any = {}
+			const champNames = Object.keys(data)
+
+			champNames.forEach(name => {
+				const champ = data[name]
+				champMap[champ.key] = champ
+			})
+
+			window.localStorage.setItem(KEY_CHAMPS, JSON.stringify(champMap))
+			window.localStorage.setItem(KEY_CHAMPS_LAST_SAVED, genTimestamp())
+
+			return champMap
+		})
+		.catch(err => {
+			alert(`Failed to fetch champs!\n\n${JSON.stringify(err, null, 4)}`)
+		})
+}
+
+const APIConfigInfo: FC<IAPIConfigInfoProps> = ({ onAPIKeySaved }) => {
+	const KEY_API_KEY = 'simpleLeagueCompare.API-dev'
 	const KEY_CHAMPS_LAST_SAVED = 'simpleLeagueCompare.saved.champs'
 
 	const [champData, setChampData] = useState(null)
 	const [devAPIKey, setDevAPIKey] = useState('')
 
-	const genTimestamp = (): string => String((new Date()).getTime())
 	const saveKeyToLocalStorage = () => {
 		window.localStorage.setItem(KEY_API_KEY, devAPIKey)
 		onAPIKeySaved(devAPIKey)
@@ -32,29 +56,10 @@ const APIConfigInfo: FC<IAPIConfigInfoProps> = ({ onAPIKeySaved }) => {
 			onAPIKeySaved(loadedDevKey)
 		}
 
-		const fetchChamps = async (): Promise<void> => {
-			return fetch(`https://ddragon.leagueoflegends.com/cdn/${API_V}/data/en_US/champion.json`)
-				.then(resp => resp.json())
-				.then(({ data }) => {
-					const champMap: any = {}
-					const champNames = Object.keys(data)
-	
-					champNames.forEach(name => {
-						const champ = data[name]
-						champMap[champ.key] = champ
-					})
-	
-					setChampData(champMap)
-					window.localStorage.setItem(KEY_CHAMPS, JSON.stringify(champMap))
-					window.localStorage.setItem(KEY_CHAMPS_LAST_SAVED, genTimestamp())
-				})
-				.catch(err => {
-					alert(`Failed to fetch champs!\n\n${JSON.stringify(err, null, 4)}`)
-				})
-		}
 		const loadedLastChamps = String(window.localStorage.getItem(KEY_CHAMPS_LAST_SAVED) || '')
+		const areLastChampsAvailable = loadedLastChamps.length > 0
 
-		if (loadedLastChamps.length > 0) {
+		if (areLastChampsAvailable) {
 			const loadedTimestamp = parseInt(loadedLastChamps, 10)
 			const lastSetOn = new Date(loadedTimestamp)
 			const now = new Date()
@@ -66,10 +71,10 @@ const APIConfigInfo: FC<IAPIConfigInfoProps> = ({ onAPIKeySaved }) => {
 			const diffInDays = (now.getTime() - lastSetOn.getTime()) / 1000 / 60 / 1440
 
 			if (diffInDays >= 1) { // NOTE: saved data is stale, must fetch champs
-				fetchChamps()
+				fetchChamps().then(champMap => { setChampData(champMap) })
 			}
 		} else { // NOTE: never saved data, must fetch champs
-			fetchChamps()
+			fetchChamps().then(champMap => { setChampData(champMap) })
 		}
 	}, [onAPIKeySaved])
 
