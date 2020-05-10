79da0a8d6450f7e179293cf1751fcabea0d5656b
Anthony Williams <anthony.williams.cs@gmail.com>
2020-05-09 19:39:54 -0600

    Create basic MatchlistDisplay

---
+++ simpleleaguecompare/src/components/MatchlistDisplay.tsx
@@ -0,0 +1,52 @@
+import React, { FC, useState } from 'react'
+// import { KEY_CHAMPS } from '../utils'
+import './App.css'
+
+const API_URL = process.env.REACT_APP_API_URL
+
+export interface IMatchlistDisplay {
+	accountKey: string
+	apiKey: string
+	playerName: string
+}
+
+const MatchlistDisplay: FC<IMatchlistDisplay> = ({ accountKey, apiKey, playerName }) => {
+	const REGION = 'na1'
+
+	const [matchlist, setMatchlist] = useState<any[]>([])
+
+	const fetchMatchlist = async (encryptedAccountKey: string): Promise<void> => {
+		return fetch(`${API_URL}/matchlist/${encryptedAccountKey}/${apiKey}`)
+			.then(response => response.json())
+			.then(matches => {
+				setMatchlist(matches)
+			})
+			.catch(err => {
+				alert(`Failed to fetch matches!\n\n${JSON.stringify(err, null, 4)}`)
+			})
+	}
+
+	return (
+		<div className="matchlist-container">
+			<p>Match list for {playerName}:</p>
+			<button onClick={() => { fetchMatchlist(accountKey) }}>Fetch {playerName}'s Matchlist (beta)</button>
+			{matchlist.length > 0 && <div className={`container-matchlist ${playerName}`}>
+				{matchlist.map(({ champion, gameId, lane, role }) => {
+					return (<div className="container-match" key={gameId}>
+						<p>Game ID:&nbsp;
+							<a
+								href={`https://${REGION}.api.riotgames.com/lol/match/v4/matches/${gameId}?api_key=${apiKey}`}
+								rel="noopener noreferrer"
+								target="_blank">{gameId}</a>
+						</p>
+						<p>Champion: {champion}</p>
+						<p>Lane: {lane}</p>
+						<p>Role: {role}</p>
+					</div>)
+				})}
+			</div>}
+		</div>
+	)
+}
+
+export { MatchlistDisplay }
