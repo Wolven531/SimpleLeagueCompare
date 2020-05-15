6b24be9679f57ef6792874b2dea18cf1206a4282
Anthony Williams <anthony.williams.cs@gmail.com>
2020-05-14 18:46:30 -0600

    Provide AppService to MatchlistController

--- api/src/matchlist/matchlist.module.ts
+++ api/src/matchlist/matchlist.module.ts
@@ -1,11 +1,13 @@
 import { HttpModule, Logger, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { AppService } from '../app.service';
 import { MatchlistController } from './matchlist.controller';
 
 @Module({
 	controllers: [MatchlistController],
 	imports: [HttpModule],
 	providers: [
+		AppService,
 		ConfigService,
 		Logger,
 	],
