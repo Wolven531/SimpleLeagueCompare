8c2abcd721f9b0b37188b016c32c6d63352d5072
Anthony Williams <anthony.williams.cs@gmail.com>
2020-05-18 20:46:21 -0600

    Optional getLastX Query to matchlist endpoint

--- api/src/matchlist/matchlist.controller.ts
+++ api/src/matchlist/matchlist.controller.ts
@@ -4,7 +4,8 @@ import {
 	Header,
 	HttpCode,
 	HttpStatus,
-	Param
+	Param,
+	Query
 } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { AppService } from '../services/app.service'
@@ -22,11 +23,16 @@ export class MatchlistController {
 	@Header('Cache-Control', 'none')
 	async getMatchlist(
 		@Param('accountId') accountId: string,
-		// NOTE: if included, response stream MUST be closed via .end()
-		// @Response() response: ExResponse
+		@Query('getLastX') getLastX: number | undefined,
 	): Promise<any[]> {
 		const apiKey = this.configService.get(ENV_API_KEY, ENV_API_KEY_DEFAULT)
-		return this.appService.getMatchlist(apiKey, accountId)
+		const allMatches = await this.appService.getMatchlist(apiKey, accountId)
+
+		if (getLastX !== undefined) {
+			return allMatches.slice(0, getLastX)
+		}
+
+		return allMatches
 	}
 
 	@Get('game/:gameId')
