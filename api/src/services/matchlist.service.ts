4effbd2befe3b83f4d91d49e4683ad88f646ac14
Anthony Williams <anthony.williams.cs@gmail.com>
2020-05-27 22:55:10 -0600

    Add getTotalMastery() to MLSvc

--- api/src/services/matchlist.service.ts
+++ api/src/services/matchlist.service.ts
@@ -19,16 +19,43 @@ export class MatchlistService {
       .then(resp => {
           const { endIndex, matches, startIndex, totalGames } = resp.data
 
-          console.log(`[ getMatchlist | app-svc ] ${totalGames} total matches, returning indices ${startIndex} - ${endIndex}`)
+          console.log(`[ getMatchlist | match-svc ] ${totalGames} total matches, returning indices ${startIndex} - ${endIndex}`)
 
           return matches
         },
         rejectionReason => {
-          console.log(`[ getMatchlist | app-svc ] Promise rejected!\n\n${JSON.stringify(rejectionReason, null, 4)}`)
+          console.log(`[ getMatchlist | match-svc ] Promise rejected!\n\n${JSON.stringify(rejectionReason, null, 4)}`)
         }
       )
       .catch(err => {
-        console.log(`[ getMatchlist | app-svc ] Error while fetching matches!\n\n${JSON.stringify(err, null, 4)}`)
+        console.log(`[ getMatchlist | match-svc ] Error while fetching matches!\n\n${JSON.stringify(err, null, 4)}`)
+      })
+  }
+
+  async getTotalMastery(apiKey: string, summonerId: string, defaultMasteryTotal = 0): Promise<number> {
+    return this.httpService.get(`https://${REGION}.api.riotgames.com/lol/champion-mastery/v4/scores/by-summoner/${summonerId}`,
+      {
+        headers: {
+          'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'Accept-Language': 'en-US,en;q=0.9',
+          'X-Riot-Token': apiKey
+        }
+      })
+      .toPromise()
+      .then(resp => {
+        const totalScore = parseInt(resp.data, 10)
+
+        console.log(`[ getTotalMastery | match-svc ] totalScore=${totalScore}`)
+
+        return totalScore
+      },
+        rejectionReason => {
+          console.log(`[ getTotalMastery | match-svc ] Promise rejected!\n\n${JSON.stringify(rejectionReason, null, 4)}`)
+          return defaultMasteryTotal
+        })
+      .catch(err => {
+        console.log(`[ getTotalMastery | match-svc ] Error while fetching total mastery score!\n\n${JSON.stringify(err, null, 4)}`)
+        return defaultMasteryTotal
       })
   }
 
