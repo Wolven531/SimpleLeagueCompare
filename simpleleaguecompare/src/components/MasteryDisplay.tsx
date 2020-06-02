b4ce23e6c072023e5c3b962463be6435ef10abe0
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-01 23:26:19 -0600

    Abstract fetchMastery out of MasteryDisplay

--- simpleleaguecompare/src/components/MasteryDisplay.tsx
+++ simpleleaguecompare/src/components/MasteryDisplay.tsx
@@ -1,28 +1,41 @@
 import React, { FC, useState, useEffect } from 'react'
 
+type FuncMasterySet = (newMastery: number) => void
+type FuncMasteryFetch = (apiUrl: string, summonerId: string, setMastery: FuncMasterySet) => Promise<void>
+
 export interface IMasteryDisplay {
 	apiUrl: string
+	fetchMastery?: FuncMasteryFetch
 	playerName: string
 	summonerId: string
 }
 
-const MasteryDisplay: FC<IMasteryDisplay> = ({ apiUrl, playerName, summonerId }) => {
+const fetchMasteryDefault = async (
+	apiUrl: string,
+	summonerId: string,
+	setMastery: FuncMasterySet,
+): Promise<void> => {
+	return fetch(`${apiUrl}/matchlist/mastery/${summonerId}`)
+		.then(response => response.json())
+		.then(totalMastery => {
+			setMastery(totalMastery)
+		})
+		.catch(err => {
+			alert(`Failed to fetch mastery!\n\n${JSON.stringify(err, null, 4)}`)
+		})
+}
+
+const MasteryDisplay: FC<IMasteryDisplay> = ({
+	apiUrl,
+	fetchMastery = fetchMasteryDefault,
+	playerName,
+	summonerId }
+) => {
 	const [mastery, setMastery] = useState(-1)
 
-	const fetchMastery = async (summonerId: string): Promise<void> => {
-		return fetch(`${apiUrl}/matchlist/mastery/${summonerId}`)
-			.then(response => response.json())
-			.then(totalMastery => {
-				setMastery(totalMastery)
-			})
-			.catch(err => {
-				alert(`Failed to fetch mastery!\n\n${JSON.stringify(err, null, 4)}`)
-			})
-	}
-
 	useEffect(() => {
-		fetchMastery(summonerId)
-	}, [apiUrl, playerName, summonerId])
+		fetchMastery(apiUrl, summonerId, setMastery)
+	}, [apiUrl, fetchMastery, playerName, summonerId])
 
 	return (
 		<div className="mastery-container">
