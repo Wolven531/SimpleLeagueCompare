c243dbf2c4ef68f0cfcde6e19f5ffa15f178fc4a
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-02 23:04:53 -0600

    Add Navbar and Router to App

--- simpleleaguecompare/src/App/App.tsx
+++ simpleleaguecompare/src/App/App.tsx
@@ -1,6 +1,13 @@
 import React, { FC, useState } from 'react'
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch
+} from 'react-router-dom'
 import { APIConfigInfo } from '../components/APIConfigInfo'
+import { MasteryDisplay } from '../components/MasteryDisplay'
 import { MatchlistDisplay } from '../components/MatchDisplay/MatchlistDisplay'
+import { Navbar } from '../Navbar/navbar.component'
 import {
 	ACCT_ENCRYPTED_ANTHONY,
 	ACCT_ENCRYPTED_NICOLE,
@@ -13,7 +20,6 @@ import {
 	SUMMONER_ID_VINNY
 } from '../utils'
 import './App.css'
-import { MasteryDisplay } from '../components/MasteryDisplay'
 
 // const styles = {
 // 	devAPIInput: { fontSize: '1.1em', fontWeight: 'bold', padding: 5, textAlign: 'center', width: '420px' }
@@ -39,71 +45,84 @@ const App: FC = () => {
 	}
 
 	return (
-		<div className="app">
-			<APIConfigInfo
-				onAPIKeySaved={updateApiKey}
-				onChampsSaved={updateChampsSaved}
-				onNumMatchesChanged={updateNumMatches}
-				/>
-			<ul>
-				<li className={isSpinning ? 'spinning' : ''}>
-					Champions:&nbsp;
-					<a
-						href={`https://ddragon.leagueoflegends.com/cdn/${API_V}/data/en_US/champion.json`}
-						rel="noopener noreferrer"
-						target="_blank"
-					>
-						//ddragon.leagueoflegends.com/cdn/{API_V}/data/en_US/champion.json
-					</a>
-				</li>
-				<li>
-					<MasteryDisplay
-						apiUrl={API_URL}
-						playerName="Anthony"
-						summonerId={SUMMONER_ID_ANTHONY}
-						/>
-					<MatchlistDisplay
-						accountKey={ACCT_ENCRYPTED_ANTHONY}
-						apiKey={devAPIKey}
-						apiUrl={API_URL}
-						champData={champData}
-						numToFetch={numMatchesToFetch}
-						playerName="Anthony"
-						/>
-				</li>
-				<li>
-					<MasteryDisplay
-						apiUrl={API_URL}
-						playerName="Nicole"
-						summonerId={SUMMONER_ID_NICOLE}
-						/>
-					<MatchlistDisplay
-						accountKey={ACCT_ENCRYPTED_NICOLE}
-						apiKey={devAPIKey}
-						apiUrl={API_URL}
-						champData={champData}
-						numToFetch={numMatchesToFetch}
-						playerName="Nicole"
-						/>
-				</li>
-				<li>
-					<MasteryDisplay
-						apiUrl={API_URL}
-						playerName="Vinny"
-						summonerId={SUMMONER_ID_VINNY}
-						/>
-					<MatchlistDisplay
-						accountKey={ACCT_ENCRYPTED_VINNY}
-						apiKey={devAPIKey}
-						apiUrl={API_URL}
-						champData={champData}
-						numToFetch={numMatchesToFetch}
-						playerName="Vinny"
-						/>
-				</li>
-			</ul>
-			<button onClick={toggleSpinMode}>Toggle Spin Mode!</button>
-		</div>
+		<Router>
+			<div className="app">
+				<Navbar/>
+				<Switch>
+					<Route path="/about">
+						<p>TODO: ABOUT</p>
+					</Route>
+					<Route path="/users">
+						<p>TODO: USERS</p>
+					</Route>
+					<Route path="/">
+						<APIConfigInfo
+							onAPIKeySaved={updateApiKey}
+							onChampsSaved={updateChampsSaved}
+							onNumMatchesChanged={updateNumMatches}
+							/>
+						<ul>
+							<li className={isSpinning ? 'spinning' : ''}>
+								Champions:&nbsp;
+								<a
+									href={`https://ddragon.leagueoflegends.com/cdn/${API_V}/data/en_US/champion.json`}
+									rel="noopener noreferrer"
+									target="_blank"
+								>
+									//ddragon.leagueoflegends.com/cdn/{API_V}/data/en_US/champion.json
+								</a>
+							</li>
+							<li>
+								<MasteryDisplay
+									apiUrl={API_URL}
+									playerName="Anthony"
+									summonerId={SUMMONER_ID_ANTHONY}
+									/>
+								<MatchlistDisplay
+									accountKey={ACCT_ENCRYPTED_ANTHONY}
+									apiKey={devAPIKey}
+									apiUrl={API_URL}
+									champData={champData}
+									numToFetch={numMatchesToFetch}
+									playerName="Anthony"
+									/>
+							</li>
+							<li>
+								<MasteryDisplay
+									apiUrl={API_URL}
+									playerName="Nicole"
+									summonerId={SUMMONER_ID_NICOLE}
+									/>
+								<MatchlistDisplay
+									accountKey={ACCT_ENCRYPTED_NICOLE}
+									apiKey={devAPIKey}
+									apiUrl={API_URL}
+									champData={champData}
+									numToFetch={numMatchesToFetch}
+									playerName="Nicole"
+									/>
+							</li>
+							<li>
+								<MasteryDisplay
+									apiUrl={API_URL}
+									playerName="Vinny"
+									summonerId={SUMMONER_ID_VINNY}
+									/>
+								<MatchlistDisplay
+									accountKey={ACCT_ENCRYPTED_VINNY}
+									apiKey={devAPIKey}
+									apiUrl={API_URL}
+									champData={champData}
+									numToFetch={numMatchesToFetch}
+									playerName="Vinny"
+									/>
+							</li>
+						</ul>
+						<button onClick={toggleSpinMode}>Toggle Spin Mode!</button>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	)
 }
 
