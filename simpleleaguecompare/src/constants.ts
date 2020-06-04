aa8b7fac7cea9beb55781e1d4f7dea38f1c45a9b
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-03 20:12:20 -0600

    Use composite constants

--- simpleleaguecompare/src/constants.ts
+++ simpleleaguecompare/src/constants.ts
@@ -5,12 +5,16 @@ export const ACCT_ENCRYPTED_VINNY = 'aME0ZGruQhV8etyYYIys4vqFarj13QyvFztnwVIHImE
 export const API_URL = String(process.env.REACT_APP_API_URL || '')
 export const API_V = '10.8.1'
 
-export const KEY_API_KEY = 'simpleLeagueCompare.API-dev'
-export const KEY_CHAMPS = 'simpleLeagueCompare.champs'
-export const KEY_CHAMPS_LAST_SAVED = 'simpleLeagueCompare.saved.champs'
-
 export const SUMMONER_ID_ANTHONY = 'hcLRomxparq_5yiMDPH-dS3iLiw4xCZhGu-pxv-uviArAiog'
 export const SUMMONER_ID_NICOLE = 'G5tCj_5KLJajQ4V6PtbYjRlY_s3lRwY0ubNe4EM-NL3pG408'
 export const SUMMONER_ID_VINNY = 'Dm9zNbtMP8bqWlSsVgkPeu-ZwuRJbjKzn-BUvO1hiyXjyfC1'
 
 export const REGION = 'na1'
+
+// NOTE: above are simple constants, below are composite constants
+
+const KEY_BASE = 'simpleLeagueCompare'
+
+export const KEY_API_KEY = `${KEY_BASE}.API-dev`
+export const KEY_CHAMPS = `${KEY_BASE}.champs`
+export const KEY_CHAMPS_LAST_SAVED = `${KEY_BASE}.saved.champs`
