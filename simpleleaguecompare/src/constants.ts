b95f1cd8cf1c4d8fb1c9cca1b0cc273ae9fa0af6
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-03 20:08:59 -0600

    Add constants file

---
+++ simpleleaguecompare/src/constants.ts
@@ -0,0 +1,16 @@
+export const ACCT_ENCRYPTED_ANTHONY = 'U9b-KVWyJkTpQR0YiPJt7U8DFqy5llDfTJZYV56-G7onXevEOMC_DiI1'
+export const ACCT_ENCRYPTED_NICOLE = 'Cn0MOwyHpDXOLaCqpbkwMoIs1M8r9IJnv39DOM867E1zTjE'
+export const ACCT_ENCRYPTED_VINNY = 'aME0ZGruQhV8etyYYIys4vqFarj13QyvFztnwVIHImEgEgiwl7OLPsRE'
+
+export const API_URL = String(process.env.REACT_APP_API_URL || '')
+export const API_V = '10.8.1'
+
+export const KEY_API_KEY = 'simpleLeagueCompare.API-dev'
+export const KEY_CHAMPS = 'simpleLeagueCompare.champs'
+export const KEY_CHAMPS_LAST_SAVED = 'simpleLeagueCompare.saved.champs'
+
+export const SUMMONER_ID_ANTHONY = 'hcLRomxparq_5yiMDPH-dS3iLiw4xCZhGu-pxv-uviArAiog'
+export const SUMMONER_ID_NICOLE = 'G5tCj_5KLJajQ4V6PtbYjRlY_s3lRwY0ubNe4EM-NL3pG408'
+export const SUMMONER_ID_VINNY = 'Dm9zNbtMP8bqWlSsVgkPeu-ZwuRJbjKzn-BUvO1hiyXjyfC1'
+
+export const REGION = 'na1'
