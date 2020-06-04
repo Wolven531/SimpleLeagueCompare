7a90fdf950ae75c78a866f7ef8ba02ba8856206e
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-03 20:16:35 -0600

    Organize constants, add name constants

--- simpleleaguecompare/src/constants.ts
+++ simpleleaguecompare/src/constants.ts
@@ -1,15 +1,20 @@
-export const ACCT_ENCRYPTED_ANTHONY = 'U9b-KVWyJkTpQR0YiPJt7U8DFqy5llDfTJZYV56-G7onXevEOMC_DiI1'
-export const ACCT_ENCRYPTED_NICOLE = 'Cn0MOwyHpDXOLaCqpbkwMoIs1M8r9IJnv39DOM867E1zTjE'
-export const ACCT_ENCRYPTED_VINNY = 'aME0ZGruQhV8etyYYIys4vqFarj13QyvFztnwVIHImEgEgiwl7OLPsRE'
-
+// NOTE: general constants
 export const API_URL = String(process.env.REACT_APP_API_URL || '')
 export const API_V = '10.8.1'
-
-export const SUMMONER_ID_ANTHONY = 'hcLRomxparq_5yiMDPH-dS3iLiw4xCZhGu-pxv-uviArAiog'
-export const SUMMONER_ID_NICOLE = 'G5tCj_5KLJajQ4V6PtbYjRlY_s3lRwY0ubNe4EM-NL3pG408'
-export const SUMMONER_ID_VINNY = 'Dm9zNbtMP8bqWlSsVgkPeu-ZwuRJbjKzn-BUvO1hiyXjyfC1'
-
 export const REGION = 'na1'
+
+// NOTE: user constants
+export const ACCT_ENCRYPTED_ANTHONY = 'U9b-KVWyJkTpQR0YiPJt7U8DFqy5llDfTJZYV56-G7onXevEOMC_DiI1'
+export const NAME_ANTHONY = 'Anthony'
+export const SUMMONER_ID_ANTHONY = 'hcLRomxparq_5yiMDPH-dS3iLiw4xCZhGu-pxv-uviArAiog'
+
+export const ACCT_ENCRYPTED_NICOLE = 'Cn0MOwyHpDXOLaCqpbkwMoIs1M8r9IJnv39DOM867E1zTjE'
+export const NAME_NICOLE = 'Nicole'
+export const SUMMONER_ID_NICOLE = 'G5tCj_5KLJajQ4V6PtbYjRlY_s3lRwY0ubNe4EM-NL3pG408'
+
+export const ACCT_ENCRYPTED_VINNY = 'aME0ZGruQhV8etyYYIys4vqFarj13QyvFztnwVIHImEgEgiwl7OLPsRE'
+export const NAME_VINNY = 'Vinny'
+export const SUMMONER_ID_VINNY = 'Dm9zNbtMP8bqWlSsVgkPeu-ZwuRJbjKzn-BUvO1hiyXjyfC1'
 
 // NOTE: above are simple constants, below are composite constants
 
