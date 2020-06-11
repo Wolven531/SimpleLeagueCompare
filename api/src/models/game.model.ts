88f23fe482764a69cff659bc817c906282d9f5b5
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-11 00:07:02 -0600

    Add param docs to team model

--- api/src/models/game.model.ts
+++ api/src/models/game.model.ts
@@ -5,13 +5,13 @@ class Game {
 	 * @param gameCreation - Timestamp of when game was created
 	 * @param gameDuration - Length of game in seconds
 	 * @param gameId - Unique identifier for game
-	 * @param gameMode - Mode of game (e.g. "CLASSIC")
-	 * @param gameType - Type of game (e.g. "MATCHED_GAME")
+	 * @param gameMode - Mode of game (e.g. 'CLASSIC')
+	 * @param gameType - Type of game (e.g. 'MATCHED_GAME')
 	 * @param gameVersion - Patch version used for game
 	 * @param mapId - Refers to which map game was played on
 	 * @param participantIdentities - Array of info related to League about participants
 	 * @param participants - Array of info related to specific game about participants
-	 * @param platformId - Which platform game was played on (e.g. "NA1")
+	 * @param platformId - Which platform game was played on (e.g. 'NA1')
 	 * @param queueId - Which queue generated game
 	 * @param seasonId - Which season game was played in
 	 * @param teams - Array describing the factions in game
--- api/src/models/team.model.ts
+++ api/src/models/team.model.ts
@@ -1,6 +1,21 @@
 class Team {
 	/**
-	 *
+	 * @param teamId - Team number used in game (i.e. 100, 200)
+	 * @param win - Whether the team achieved a victory (e.g. 'Win')
+	 * @param firstBlood - True if this team scored the first kill in the game, false otherwise
+	 * @param firstTower - True if this team destroyed the first tower in the game, false otherwise
+	 * @param firstInhibitor - True if this team destroyed the first inhibitor in the game, false otherwise
+	 * @param firstBaron - True if this team scored the first baron monster kill in the game, false otherwise
+	 * @param firstDragon - True if this team scored the first dragon monster kill in the game, false otherwise
+	 * @param firstRiftHerald - True if this team scored the first rift herald monster kill in the game, false otherwise
+	 * @param towerKills - Number of towers this team destroyed in the game
+	 * @param inhibitorKills - Number of inhibitors this team destroyed in the game
+	 * @param baronKills - Number of baron monster kills this team scored in the game
+	 * @param dragonKills - Number of dragon monster kills this team scored in the game
+	 * @param vilemawKills - Number of vile maw monster kills this team scored in the game
+	 * @param riftHeraldKills - Number of rift herald monster kills this team scored in the game
+	 * @param dominionVictoryScore - Score achieved if the game was dominion
+	 * @param bans - array of champions banned from the game in pre-game setup
 	 */
 	constructor(
 		public teamId: number,
