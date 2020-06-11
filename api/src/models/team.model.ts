e30c2ab56d6313755b139b2190fae92f8b5d7954
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-11 00:01:17 -0600

    Add ctor props to Team model

--- api/src/models/team.model.ts
+++ api/src/models/team.model.ts
@@ -1,5 +1,25 @@
 class Team {
-
+	/**
+	 *
+	 */
+	constructor(
+		public teamId: number,
+		public win: string,
+		public firstBlood: boolean,
+		public firstTower: boolean,
+		public firstInhibitor: boolean,
+		public firstBaron: boolean,
+		public firstDragon: boolean,
+		public firstRiftHerald: boolean,
+		public towerKills: number,
+		public inhibitorKills: number,
+		public baronKills: number,
+		public dragonKills: number,
+		public vilemawKills: number,
+		public riftHeraldKills: number,
+		public dominionVictoryScore: number,
+		public bans: any[],
+	) {}
 }
 
 export { Team }
