aace7d372592c555d94e3c637682f2a38bfc53a6
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-14 23:00:18 -0600

    Add ParticipantIdentity model

---
+++ api/src/models/participant-identity.model.ts
@@ -0,0 +1,14 @@
+import { Player } from './player.model';
+
+class ParticipantIdentity {
+	/**
+	 * @param participantId - Number of the participant in game (e.g. 1-10 on SR, 1-9 on TFT)
+	 * @param player - Object containing player information for this participant
+	 */
+	constructor(
+		public participantId: number,
+		public player: Player,
+	) {}
+}
+
+export { ParticipantIdentity }
