3140c2a124008359e28aa47ea28c84554bebbf25
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-14 22:57:04 -0600

    Create player model

---
+++ api/src/models/player.model.ts
@@ -0,0 +1,24 @@
+class Player {
+	/**
+	 * @param platformId - Original region player exists in (e.g. 'NA')
+	 * @param accountId - Original encrypted account ID for player
+	 * @param summonerName - Name displayed for player in game
+	 * @param summonerId - Simple unique summoner ID for player
+	 * @param currentPlatformId - Current region player exists in (equal to `platformId` in most cases)
+	 * @param currentAccountId - Current encrypted account ID for player (equal to `accountId` in most cases)
+	 * @param matchHistoryUri - Unique URI used to fetch the match history for player
+	 * @param profileIcon - Unique identifier for the icon selected by player
+	 */
+	constructor(
+		public platformId: string,
+		public accountId: string,
+		public summonerName: string,
+		public summonerId: string,
+		public currentPlatformId: string,
+		public currentAccountId: string,
+		public matchHistoryUri: string,
+		public profileIcon: number,
+	) {}
+}
+
+export { Player }
