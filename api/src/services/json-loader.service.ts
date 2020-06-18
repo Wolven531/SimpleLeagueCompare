54f756531a61a63f6e45b816d02e401d62a75afc
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-17 22:22:47 -0600

    Start loading users file in loadUsersFromFile

--- api/src/services/json-loader.service.ts
+++ api/src/services/json-loader.service.ts
@@ -4,6 +4,8 @@ import {
 	Logger,
 	LoggerService
 } from '@nestjs/common'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 import { User } from '../models/user.model'
 
 @Injectable()
@@ -16,12 +18,17 @@ export class JsonLoaderService {
 	getUserByFriendlyName(friendlyName: string): User | undefined {
 		const searchKey = friendlyName.toLowerCase()
 		const users = this.loadUsersFromFile()
+
 		this.logger.log(`Searching for friendlyName = "${searchKey}"`, ' getUserByFriendlyName | json-loader-svc ')
 
 		return users.find(u => u.name.toLowerCase() === searchKey)
 	}
 
 	loadUsersFromFile(): User[] {
+		const fileContents = readFileSync(join(__dirname, '..', 'data', 'users.json'))
+
+		this.logger.log(`fileContents=\n\n${fileContents}\n`, ' loadUsersFromFile | json-loader-svc ')
+
 		return []
 	}
 }
