ce06cdc8f2617c39c6e3ce7d1dcb2e73be4cbce8
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-21 22:16:34 -0600

    Add totalMastery to User model

--- api/src/models/user.model.ts
+++ api/src/models/user.model.ts
@@ -1,11 +1,13 @@
 class User {
 	/**
 	 * @param accountId - Encrypted account ID for the user
+	 * @param masteryTotal - Total mastery points the user has earned
 	 * @param name - Friendly name of the user
 	 * @param summonerId - Simple summoner ID for the user
 	 */
 	constructor(
 		public accountId: string,
+		public totalMastery: number,
 		public name: string,
 		public summonerId: string,
 	) {}
