28f0b69137412059db80506eff0a423b056e82fd
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-27 18:12:00 -0600

    Add isFresh getter to User model

--- api/src/models/user.model.ts
+++ api/src/models/user.model.ts
@@ -1,7 +1,7 @@
 class User {
 	/**
 	 * @param accountId - Encrypted account ID for the user
-	 * @param lastUpdated - Timestamp of the last update run time for the user
+	 * @param lastUpdated - UTC Timestamp of the last update run time for the user
 	 * @param masteryTotal - Total mastery points the user has earned
 	 * @param name - Friendly name of the user
 	 * @param summonerId - Simple summoner ID for the user
@@ -13,6 +13,18 @@ class User {
 		public name: string,
 		public summonerId: string,
 	) {}
+
+	/**
+	 * @returns True if user model has been updated within the last day; false otherwise
+	 */
+	get isFresh(): boolean {
+		const now = new Date()
+		const nowUtc = Date.UTC(now.getFullYear(), now.getMonth())
+		const diff = nowUtc - this.lastUpdated
+
+		// NOTE: if diff in time is less than or equal to 24 hours (i.e. one day)
+		return diff <= (1000 * 60 * 60 * 24)
+	}
 }
 
 export { User }
