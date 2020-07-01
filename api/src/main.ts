222e28c48df5c36d118b216bb5387ade1c14eac9
Anthony Williams <anthony.williams.cs@gmail.com>
2020-06-30 20:12:34 -0600

    Refresh users during API startup

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,6 +2,7 @@ import { Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
+import { MatchlistService } from './services/matchlist.service'
 import {
 	ENV_API_KEY,
 	ENV_API_KEY_DEFAULT,
@@ -13,12 +14,18 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule, { cors: true })
 	const configService = app.get(ConfigService)
 	const logger = app.get(Logger);
+	const matchlistService = app.get(MatchlistService)
 
 	// NOTE: get values from ConfigService, which uses env files and vars
 	const envApiKey = configService.get(ENV_API_KEY, ENV_API_KEY_DEFAULT)
 	const port = configService.get(ENV_API_PORT, ENV_API_PORT_DEFAULT)
 
 	logger.debug(`Loaded apiKey from env=\t${envApiKey}`, 'bootstrap | main')
+	logger.log(`About to refresh users...`, 'bootstrap | main')
+
+	const updatedUsers = await matchlistService.refreshMasteryTotalForAllUsers(envApiKey)
+
+	logger.log(`Updated ${updatedUsers.length} users`, 'bootstrap | main')
 	logger.log(`Starting to listen for NestJS app on port ${port}...`, 'bootstrap | main')
 
 	await app.listen(port)
