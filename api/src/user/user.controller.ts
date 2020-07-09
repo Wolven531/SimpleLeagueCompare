a880d435352f10ad0ffc161f61f0cb908e972f55
Anthony Williams <anthony.williams.cs@gmail.com>
2020-07-08 22:47:28 -0600

    Implement user refresh endpoint logic

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -4,9 +4,14 @@ import {
 	Header,
 	HttpCode,
 	HttpStatus,
+	Inject,
+	Logger,
+	LoggerService,
 	// Param,
 	// Query
 } from '@nestjs/common'
+import { execFileSync } from 'child_process'
+import { join } from 'path'
 // import { ConfigService } from '@nestjs/config'
 // import { MatchlistService } from '../services/matchlist.service'
 // import {
@@ -19,6 +24,8 @@ export class UserController {
 	constructor(
 		// private readonly matchlistService: MatchlistService,
 		// private readonly configService: ConfigService,
+		@Inject(Logger)
+		private readonly logger: LoggerService
 	) { }
 
 	@Get('refresh')
@@ -26,6 +33,25 @@ export class UserController {
 	@Header('Cache-Control', 'none')
 	async refreshUserData(): Promise<string> {
 		// const apiKey = this.configService.get(ENV_API_KEY, ENV_API_KEY_DEFAULT)
+
+		const dirContainingPackage = join(__dirname, '..', '..')
+		this.logger.warn(
+			`About to execute script in dir "${dirContainingPackage}" ...`,
+			' refreshUserData | user-ctrl ')
+
+		execFileSync(
+			'npm',
+			[
+				'run',
+				'copy:users:windows'
+			],
+			{
+				cwd: dirContainingPackage,
+				// TODO: research how to use `shell` property
+				// shell: true,
+			})
+
+		this.logger.warn(`Script completed`, ' refreshUserData | user-ctrl ')
 
 		// return this.matchlistService.getMasteryTotal(apiKey, summonerId)
 		return 'OK'
