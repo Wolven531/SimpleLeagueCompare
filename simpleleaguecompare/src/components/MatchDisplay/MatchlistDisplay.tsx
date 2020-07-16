26b85123a96b0308059da9c35a913af303f991c4
Anthony Williams <anthony.williams.cs@gmail.com>
2020-07-15 19:47:00 -0600

    Clarify types and use StatsDisplay

--- simpleleaguecompare/src/components/MatchDisplay/MatchlistDisplay.tsx
+++ simpleleaguecompare/src/components/MatchDisplay/MatchlistDisplay.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from 'react'
 import { Game } from '@models/game.model'
 import { Match } from '@models/match.model'
 import { REGION } from '../../constants'
+import { StatsDisplay } from '../StatsDisplay'
 import './match-display.css'
 
 export interface IMatchlistDisplay {
@@ -17,7 +18,7 @@ const MatchlistDisplay: FC<IMatchlistDisplay> = ({ accountKey, apiKey, apiUrl, c
 	const hasChampData = champData !== null
 	const includeGameData = numToFetch <= 5
 
-	const [matchlist, setMatchlist] = useState<any[]>([])
+	const [matchlist, setMatchlist] = useState<Game[] | Match[]>([])
 
 	const fetchMatchlist = async (encryptedAccountKey: string): Promise<void> => {
 		return fetch(`${apiUrl}/matchlist/${encryptedAccountKey}?getLastX=${numToFetch}&includeGameData=${includeGameData}`)
@@ -35,19 +36,22 @@ const MatchlistDisplay: FC<IMatchlistDisplay> = ({ accountKey, apiKey, apiUrl, c
 			<p>Match list for {playerName}:</p>
 			<button onClick={() => { fetchMatchlist(accountKey) }}>Fetch {playerName}'s Matchlist</button>
 			{matchlist.length > 0 && <div className={`container-matchlist ${playerName}`}>
-				{includeGameData && matchlist.map((game: Game) => {
-					console.log(JSON.stringify(game.participants, null, 2))
+				{includeGameData && <div>
+					<StatsDisplay games={matchlist as Game[]} />
+					{(matchlist as Game[]).map((game: Game) => {
+						console.log(JSON.stringify(game.participants, null, 2))
 
-					return (<div className="container-match" key={game.gameId}>
-						<p>Game ID:&nbsp;
-							<a
-								href={`https://${REGION}.api.riotgames.com/lol/match/v4/matches/${game.gameId}?api_key=${apiKey}`}
-								rel="noopener noreferrer"
-								target="_blank">{game.gameId}</a>
-						</p>
-					</div>)
-				})}
-				{!includeGameData && matchlist.map((match: Match) => {
+						return (<div className="container-match" key={game.gameId}>
+							<p>Game ID:&nbsp;
+								<a
+									href={`https://${REGION}.api.riotgames.com/lol/match/v4/matches/${game.gameId}?api_key=${apiKey}`}
+									rel="noopener noreferrer"
+									target="_blank">{game.gameId}</a>
+							</p>
+						</div>)
+					})}
+				</div>}
+				{!includeGameData && (matchlist as Match[]).map((match: Match) => {
 					const specificChamp = hasChampData && champData[match.champion]
 
 					return (<div className="container-match" key={match.gameId}>
