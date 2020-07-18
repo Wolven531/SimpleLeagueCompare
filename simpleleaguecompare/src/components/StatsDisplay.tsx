b5d55d7c669da27c1a340915ec87550c68d15823
Anthony Williams <anthony.williams.cs@gmail.com>
2020-07-17 21:32:31 -0600

    Display total time played

--- simpleleaguecompare/src/components/StatsDisplay.tsx
+++ simpleleaguecompare/src/components/StatsDisplay.tsx
@@ -6,9 +6,12 @@ export interface IStatsDisplay {
 }
 
 const StatsDisplay: FC<IStatsDisplay> = ({ games }) => {
+	const totalTimePlayed = games.map(g => g.gameDuration).reduce((acc, curr) => acc + curr)
+
 	return (
 		<div className="stats-display">
 			<h3>Games: {games.length}</h3>
+			<h3>Total time played: {totalTimePlayed} seconds</h3>
 		</div>
 	)
 }
