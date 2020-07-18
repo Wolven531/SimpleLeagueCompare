8ded62099b29291d8e5198314c61554c95648436
Anthony Williams <anthony.williams.cs@gmail.com>
2020-07-17 21:41:42 -0600

    Display avg game length

--- simpleleaguecompare/src/components/StatsDisplay.tsx
+++ simpleleaguecompare/src/components/StatsDisplay.tsx
@@ -8,11 +8,13 @@ export interface IStatsDisplay {
 
 const StatsDisplay: FC<IStatsDisplay> = ({ games }) => {
 	const totalTimePlayed = games.map(g => g.gameDuration).reduce((acc, curr) => acc + curr)
+	const avgTimePlayed = totalTimePlayed / games.length
 
 	return (
 		<div className="stats-display">
 			<h3>Games: {games.length}</h3>
 			<h3>Total time played: {moment.duration(totalTimePlayed, "seconds").asHours().toFixed(2)} hours</h3>
+			<h3>Avg. game length: {moment.duration(avgTimePlayed, "seconds").asMinutes().toFixed(2)} minutes</h3>
 		</div>
 	)
 }
