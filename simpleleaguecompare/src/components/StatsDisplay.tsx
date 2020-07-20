7daae97e87ddd61a28dcc8b5e697b9af53faeb44
Anthony Williams <anthony.williams.cs@gmail.com>
2020-07-19 20:53:07 -0600

    Display total gold earned

--- simpleleaguecompare/src/components/StatsDisplay.tsx
+++ simpleleaguecompare/src/components/StatsDisplay.tsx
@@ -4,32 +4,36 @@ import { Game } from '@models/game.model'
 
 export interface IStatsDisplay {
 	games: Game[]
+	targetAccountKey: string
 }
 
-const StatsDisplay: FC<IStatsDisplay> = ({ games }) => {
+const StatsDisplay: FC<IStatsDisplay> = ({ games, targetAccountKey }) => {
 	const totalTimePlayed = games.map(g => g.gameDuration).reduce((acc, curr) => acc + curr)
 	const avgTimePlayed = totalTimePlayed / games.length
+	let totalGoldEarned = 0
+
+	games.forEach(g => {
+		g.participantIdentities.forEach(p => {
+			if (p.player.currentAccountId !== targetAccountKey) {
+				return
+			}
+
+			const participant = g.participants.find(par => par.participantId === p.participantId)
+
+			if (!participant) {
+				return
+			}
+
+			totalGoldEarned += participant.stats.goldEarned
+		})
+	})
 
 	return (
 		<div className="stats-display">
 			<h3>Games: {games.length}</h3>
 			<h3>Total time played: {moment.duration(totalTimePlayed, "seconds").asHours().toFixed(2)} hours</h3>
 			<h3>Avg. game length: {moment.duration(avgTimePlayed, "seconds").asMinutes().toFixed(2)} minutes</h3>
-			<h3>Participants</h3>
-			<ol>
-				<li>
-					<h4>Team 1</h4>
-					<ol>
-						<li></li>
-					</ol>
-				</li>
-				<li>
-					<h4>Team 2</h4>
-					<ol>
-						<li></li>
-					</ol>
-				</li>
-			</ol>
+			<h3>Total gold earned: {totalGoldEarned}</h3>
 		</div>
 	)
 }
