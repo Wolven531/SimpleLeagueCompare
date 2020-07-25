ba3b49cee750144e9a737ce002aba13466016220
Anthony Williams <anthony.williams.cs@gmail.com>
2020-07-24 20:34:15 -0600

    Add stats summary endpoint

--- api/src/stats/stats.controller.ts
+++ api/src/stats/stats.controller.ts
@@ -1,11 +1,38 @@
-import { Controller } from '@nestjs/common'
+import {
+	Controller,
+	Get,
+	Header,
+	HttpCode,
+	HttpStatus,
+	Inject,
+	Logger,
+	LoggerService,
+	Query
+} from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { MatchlistService } from '../services/matchlist.service'
+import { ENV_API_KEY, ENV_API_KEY_DEFAULT } from '../constants'
 
 @Controller('stats')
 export class StatsController {
 	constructor(
 		private readonly matchlistService: MatchlistService,
 		private readonly configService: ConfigService,
+		@Inject(Logger)
+		private readonly logger: LoggerService,
 	) { }
+
+	@Get('summary')
+	@HttpCode(HttpStatus.OK)
+	@Header('Cache-Control', 'none')
+	async getSummary(
+		@Query('summonerId') summonerId: string | undefined,
+	): Promise<any> {
+		const apiKey = this.configService.get(ENV_API_KEY, ENV_API_KEY_DEFAULT)
+
+		return {
+			apiKey,
+			summonerId,
+		}
+	}
 }
