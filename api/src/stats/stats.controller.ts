acff7b3125fe586a08efbfd45d9ca26d3a5efa2f
Anthony Williams <anthony.williams.cs@gmail.com>
2020-07-26 18:21:41 -0600

    Add StatsSvc, CalculatedStats to summary endpoint

--- api/src/stats/stats.controller.ts
+++ api/src/stats/stats.controller.ts
@@ -11,7 +11,10 @@ import {
 	Query
 } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { CalculatedStats } from '@models/calculated-stats.model'
+import { Game } from '@models/game.model'
 import { MatchlistService } from '../services/matchlist.service'
+import { StatsService } from '../services/stats.service'
 import { ENV_API_KEY, ENV_API_KEY_DEFAULT } from '../constants'
 
 @Controller('stats')
@@ -19,6 +22,7 @@ export class StatsController {
 	constructor(
 		private readonly matchlistService: MatchlistService,
 		private readonly configService: ConfigService,
+		private readonly statsService: StatsService,
 		@Inject(Logger)
 		private readonly logger: LoggerService,
 	) { }
@@ -30,7 +34,7 @@ export class StatsController {
 		@Query('accountId') accountId: string | undefined,
 		@Query('getLastX') getLastX: number | undefined,
 		@Query('includeGameData') includeGameData: boolean = false,
-	): Promise<any> {
+	): Promise<CalculatedStats> {
 		if (!accountId || accountId.length < 1) {
 			throw new BadRequestException({
 				error: true,
@@ -48,11 +52,6 @@ export class StatsController {
 
 		const matches = await this.matchlistService.getMatchlist(apiKey, accountId, getLastX, includeGameData)
 
-		return {
-			accountId,
-			getLastX,
-			includeGameData,
-			matches,
-		}
+		return this.statsService.calculateGeneralStats(accountId, matches as Game[])
 	}
 }
