308b990132f0478c731a71d81c92072fed01fb63
Anthony Williams <anthony.williams.cs@gmail.com>
2020-07-27 22:11:04 -0600

    Add FuncStatsFetch to common-types

--- simpleleaguecompare/src/common-types.ts
+++ simpleleaguecompare/src/common-types.ts
@@ -1,1 +1,4 @@
+import { CalculatedStats } from '@models/calculated-stats.model'
+
 export type FuncMasteryFetch = (apiUrl: string, summonerId: string, defaultTotalMastery?: number) => Promise<number>
+export type FuncStatsFetch = (apiUrl: string, summonerId: string, defaultCalculatedStats?: CalculatedStats) => Promise<CalculatedStats>
