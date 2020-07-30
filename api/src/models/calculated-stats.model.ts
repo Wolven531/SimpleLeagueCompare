cca679ccb094899f59e3cca22ce83560f22ec3c4
Anthony Williams <anthony.williams.cs@gmail.com>
2020-07-29 19:12:48 -0600

    Add assists avg, deaths avg, kills avg

--- api/src/models/calculated-stats.model.ts
+++ api/src/models/calculated-stats.model.ts
@@ -6,8 +6,11 @@ class CalculatedStats {
 	 * @param kDA - calculated by adding kills and assists and dividing by assists across all games
 	 * @param timePlayedAvg - average amount of seconds per game
 	 * @param timePlayedTotal - total amount of seconds across all games
+	 * @param assistsAvg - average amount of assists earned per game
 	 * @param assistsTotal - total amount of assists earned across all games
+	 * @param deathsAvg - average amount of deaths earned per game
 	 * @param deathsTotal - total amount of deaths earned across all games
+	 * @param killsAvg - average amount of kills earned per game
 	 * @param killsTotal - total amount of kills earned across all games
 	 * @param totalLosses - total number of losses across all games
 	 * @param totalWins - total number of wins across all games
@@ -20,8 +23,11 @@ class CalculatedStats {
 		public kDA: number,
 		public timePlayedAvg: number,
 		public timePlayedTotal: number,
+		public assistsAvg: number,
 		public assistsTotal: number,
+		public deathsAvg: number,
 		public deathsTotal: number,
+		public killsAvg: number,
 		public killsTotal: number,
 		public totalLosses: number,
 		public totalWins: number,
