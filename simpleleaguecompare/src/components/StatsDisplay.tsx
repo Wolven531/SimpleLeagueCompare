71931f490c4bafd388fb116abf0380e4cc4ff399
Anthony Williams <anthony.williams.cs@gmail.com>
2020-07-29 19:36:47 -0600

    Fetch stats from API

--- simpleleaguecompare/src/components/StatsDisplay.tsx
+++ simpleleaguecompare/src/components/StatsDisplay.tsx
@@ -1,24 +1,26 @@
 import moment from 'moment'
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { CalculatedStats } from '@models/calculated-stats.model'
-import { Game } from '@models/game.model'
 import { FuncStatsFetch } from '../common-types'
 import { FORMATTER_NUMBER_FRACTION, FORMATTER_NUMBER_WHOLE } from '../constants'
 
 export interface IStatsDisplay {
-	games: Game[]
-	targetAccountKey: string
+	accountId: string
+	apiUrl: string
+	fetchStats?: FuncStatsFetch
 }
 
 const TOKEN_COMP = 'StatsDisplay'
 
+const DEFAULT_CALC_STATS = new CalculatedStats(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
 const defaultFetchStats: FuncStatsFetch = (
+	accountId: string,
 	apiUrl: string,
-	summonerId: string,
-	defaultCalculatedStats = new CalculatedStats(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+	defaultCalculatedStats = DEFAULT_CALC_STATS,
 ): Promise<CalculatedStats> => {
 	const TOKEN_FUNC = `[ fetchStatsDefault | ${TOKEN_COMP} ]`
-	const url = `${apiUrl}/stats/summary?${summonerId}`
+	const url = `${apiUrl}/stats/summary?accountId=${accountId}&getLastX=${5}&includeGameData=${String(true)}`
 
 	return fetch(url)
 		.then(response => response.json())
@@ -36,50 +38,25 @@ const defaultFetchStats: FuncStatsFetch = (
 		})
 }
 
-const StatsDisplay: FC<IStatsDisplay> = ({ games, targetAccountKey }) => {
-	const totalTimePlayed = games.map(g => g.gameDuration).reduce((acc, curr) => acc + curr)
-	const avgTimePlayed = totalTimePlayed / games.length
-	let totalAssists = 0
-	let totalDeaths = 0
-	let totalGoldEarned = 0
-	let totalKills = 0
-	let totalWins = 0
+const StatsDisplay: FC<IStatsDisplay> = ({ accountId, apiUrl, fetchStats = defaultFetchStats }) => {
+	const [stats, setStats] = useState(DEFAULT_CALC_STATS)
 
-	games.forEach(g => {
-		const identity = g.participantIdentities.find(i => i.player.currentAccountId === targetAccountKey);
+	useEffect(() => {
+		fetchStats(apiUrl, accountId).then(setStats)
+	}, [apiUrl, fetchStats, accountId])
 
-		if (!identity) {
-			return
-		}
-
-		const participant = g.participants.find(p => p.participantId === identity.participantId)
-
-		if (!participant) {
-			return
-		}
-
-		totalAssists += participant.stats.assists
-		totalDeaths += participant.stats.deaths
-		totalGoldEarned += participant.stats.goldEarned
-		totalKills += participant.stats.kills
-		totalWins += participant.stats.win ? 1 : 0
-	})
-
-	const avgGoldEarned = totalGoldEarned / games.length
-	const avgKDA = (totalKills + totalAssists) / totalDeaths
-
-	const displayGamesCount = FORMATTER_NUMBER_WHOLE.format(games.length)
-	const displayGoldAvg = FORMATTER_NUMBER_FRACTION.format(avgGoldEarned)
-	const displayGoldTotal = FORMATTER_NUMBER_WHOLE.format(totalGoldEarned)
-	const displayKDA = FORMATTER_NUMBER_FRACTION.format(avgKDA)
-	const displayTimePlayedAvg = FORMATTER_NUMBER_FRACTION.format(moment.duration(avgTimePlayed, 'seconds').asMinutes())
-	const displayTimePlayedTotal = FORMATTER_NUMBER_FRACTION.format(moment.duration(totalTimePlayed, 'seconds').asHours())
-	const displayTotalAssists = FORMATTER_NUMBER_WHOLE.format(totalAssists)
-	const displayTotalDeaths = FORMATTER_NUMBER_WHOLE.format(totalDeaths)
-	const displayTotalKills = FORMATTER_NUMBER_WHOLE.format(totalKills)
-	const displayTotalLosses = FORMATTER_NUMBER_WHOLE.format(games.length - totalWins)
-	const displayTotalWins = FORMATTER_NUMBER_WHOLE.format(totalWins)
-	const displayWinPercentage = FORMATTER_NUMBER_FRACTION.format(totalWins / games.length * 100)
+	const displayGamesCount = FORMATTER_NUMBER_WHOLE.format(stats.gamesCount)
+	const displayGoldAvg = FORMATTER_NUMBER_FRACTION.format(stats.goldEarnedAvg)
+	const displayGoldTotal = FORMATTER_NUMBER_WHOLE.format(stats.goldEarnedTotal)
+	const displayKDA = FORMATTER_NUMBER_FRACTION.format(stats.kDA)
+	const displayTimePlayedAvg = FORMATTER_NUMBER_FRACTION.format(moment.duration(stats.timePlayedAvg, 'seconds').asMinutes())
+	const displayTimePlayedTotal = FORMATTER_NUMBER_FRACTION.format(moment.duration(stats.timePlayedTotal, 'seconds').asHours())
+	const displayTotalAssists = FORMATTER_NUMBER_WHOLE.format(stats.assistsTotal)
+	const displayTotalDeaths = FORMATTER_NUMBER_WHOLE.format(stats.deathsTotal)
+	const displayTotalKills = FORMATTER_NUMBER_WHOLE.format(stats.killsTotal)
+	const displayTotalLosses = FORMATTER_NUMBER_WHOLE.format(stats.totalLosses)
+	const displayTotalWins = FORMATTER_NUMBER_WHOLE.format(stats.totalWins)
+	const displayWinPercentage = FORMATTER_NUMBER_FRACTION.format(stats.winPercentage)
 
 	return (
 		<div className="stats-display">
