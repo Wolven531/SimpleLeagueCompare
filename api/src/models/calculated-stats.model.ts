e2ce4356e4d28fb63278f6310a0eab39f8a40a8c
Anthony Williams <anthony.williams.cs@gmail.com>
2020-08-22 16:13:57 -0600

    Annotate calc stats model for Swagger

--- api/src/models/calculated-stats.model.ts
+++ api/src/models/calculated-stats.model.ts
@@ -1,4 +1,51 @@
+import { ApiProperty } from '@nestjs/swagger'
+
 class CalculatedStats {
+	@ApiProperty()
+	gamesCount: number
+
+	@ApiProperty()
+	goldEarnedAvg: number
+
+	@ApiProperty()
+	goldEarnedTotal: number
+
+	@ApiProperty()
+	kDA: number
+
+	@ApiProperty()
+	timePlayedAvg: number
+
+	@ApiProperty()
+	timePlayedTotal: number
+
+	@ApiProperty()
+	assistsAvg: number
+
+	@ApiProperty()
+	assistsTotal: number
+
+	@ApiProperty()
+	deathsAvg: number
+
+	@ApiProperty()
+	deathsTotal: number
+
+	@ApiProperty()
+	killsAvg: number
+
+	@ApiProperty()
+	killsTotal: number
+
+	@ApiProperty()
+	totalLosses: number
+
+	@ApiProperty()
+	totalWins: number
+
+	@ApiProperty()
+	winPercentage: number
+
 	/**
 	 * @param gamesCount - number of games used to calculate the stats in this instance
 	 * @param goldEarnedAvg - average amount of gold earned per game
@@ -17,22 +64,38 @@ class CalculatedStats {
 	 * @param winPercentage - ratio of wins to total number games
 	 */
 	constructor(
-		public gamesCount: number,
-		public goldEarnedAvg: number,
-		public goldEarnedTotal: number,
-		public kDA: number,
-		public timePlayedAvg: number,
-		public timePlayedTotal: number,
-		public assistsAvg: number,
-		public assistsTotal: number,
-		public deathsAvg: number,
-		public deathsTotal: number,
-		public killsAvg: number,
-		public killsTotal: number,
-		public totalLosses: number,
-		public totalWins: number,
-		public winPercentage: number,
-	) {}
+		gamesCount: number,
+		goldEarnedAvg: number,
+		goldEarnedTotal: number,
+		kDA: number,
+		timePlayedAvg: number,
+		timePlayedTotal: number,
+		assistsAvg: number,
+		assistsTotal: number,
+		deathsAvg: number,
+		deathsTotal: number,
+		killsAvg: number,
+		killsTotal: number,
+		totalLosses: number,
+		totalWins: number,
+		winPercentage: number,
+	) {
+		this.gamesCount = gamesCount
+		this.goldEarnedAvg = goldEarnedAvg
+		this.goldEarnedTotal = goldEarnedTotal
+		this.kDA = kDA
+		this.timePlayedAvg = timePlayedAvg
+		this.timePlayedTotal = timePlayedTotal
+		this.assistsAvg = assistsAvg
+		this.assistsTotal = assistsTotal
+		this.deathsAvg = deathsAvg
+		this.deathsTotal = deathsTotal
+		this.killsAvg = killsAvg
+		this.killsTotal = killsTotal
+		this.totalLosses = totalLosses
+		this.totalWins = totalWins
+		this.winPercentage = winPercentage
+	}
 }
 
 export { CalculatedStats }
