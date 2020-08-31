b8668df7e2b5ed673c96f7d07728f21005fd11ba
Anthony Williams <anthony.williams.cs@gmail.com>
2020-08-30 19:38:04 -0600

    Start config controller unit tests

---
+++ api/src/config/config.controller.spec.ts
@@ -0,0 +1,46 @@
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Test, TestingModule } from '@nestjs/testing'
+import { ConfigController } from './config.controller'
+
+describe('ConfigController', () => {
+	let controller: ConfigController
+	let testModule: TestingModule
+
+	beforeEach(async () => {
+		testModule = await Test.createTestingModule({
+			controllers: [ConfigController],
+			imports: [],
+			providers: [
+				{
+					provide: ConfigService,
+					useFactory: () => {
+						return {
+							get: jest.fn().mockReturnValueOnce('some-api-key'),
+						}
+					},
+				},
+				Logger,
+			],
+		}).compile()
+
+		controller = testModule.get(ConfigController)
+	})
+
+	afterEach(async () => {
+		await testModule.close()
+	})
+
+	describe('invoke getMasteryTotal w/ empty string', () => {
+		let resp: {}
+		
+		beforeEach(async () => {
+			resp = await controller.getConfig()
+		})
+
+		it('returns object w/ riotSecret property', () => {
+			expect(resp).toBeDefined()
+			expect((resp as any).riotSecret).toBeDefined()
+		})
+	})
+})
