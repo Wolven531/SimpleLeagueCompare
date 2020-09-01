0bfaf08f687187cf25471f36e5721241e2a2194c
Anthony Williams <anthony.williams.cs@gmail.com>
2020-08-31 21:29:06 -0600

    Add happy path unit test for stats controller

--- api/src/stats/stats.controller.spec.ts
+++ api/src/stats/stats.controller.spec.ts
@@ -1,4 +1,5 @@
 import { CalculatedStats } from '@models/calculated-stats.model'
+import { Game } from '@models/game.model'
 import { BadRequestException, HttpModule, Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
@@ -46,6 +47,39 @@ describe('StatsController', () => {
 		it('returns BadRequestException', () => {
 			expect(resp).toBeUndefined()
 			expect(capturedError).toBeInstanceOf(BadRequestException)
+		})
+	})
+
+	describe('invoke getSummary("someAccountId", undefined)', () => {
+		let capturedError: Error
+		let mockCalculateGeneralStats: jest.Mock
+		let mockGetMatchlist: jest.Mock
+		let resp: CalculatedStats
+
+		beforeEach(async () => {
+			mockCalculateGeneralStats = jest.fn(
+				(targetAccountKey, games) => ({ kDA: 3.14 } as CalculatedStats)
+			)
+			mockGetMatchlist = jest.fn(
+				(apiKey, accountId, getLastX, includeGameData) => Promise.resolve([] as Array<Game>)
+			)
+
+			jest.spyOn(testModule.get(MatchlistService), 'getMatchlist')
+				.mockImplementationOnce(mockGetMatchlist)
+			jest.spyOn(testModule.get(StatsService), 'calculateGeneralStats')
+				.mockImplementationOnce(mockCalculateGeneralStats)
+
+			try {
+				resp = await controller.getSummary('someAccountId', undefined)
+			} catch (err) {
+				capturedError = err
+			}
+		})
+
+		it('invokes MatchlistSvc.getMatchlist() and StatsSvc.calculateGeneralStats(), does NOT throw error', () => {
+			expect(capturedError).toBeUndefined()
+			expect(mockCalculateGeneralStats).toHaveBeenCalledTimes(1)
+			expect(mockGetMatchlist).toHaveBeenCalledTimes(1)
 		})
 	})
 })
