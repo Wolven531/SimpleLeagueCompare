6e1b163cb9a1f0f672b02c0adeabef6327f82436
Anthony Williams <anthony.williams.cs@gmail.com>
2020-09-01 21:27:56 -0600

    Clean up user controller

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -6,24 +6,14 @@ import {
 	HttpStatus,
 	Inject,
 	Logger,
-	LoggerService,
-	// Param,
-	// Query
+	LoggerService
 } from '@nestjs/common'
 import { execFileSync } from 'child_process'
 import { join } from 'path'
-// import { ConfigService } from '@nestjs/config'
-// import { MatchlistService } from '../services/matchlist.service'
-// import {
-// 	ENV_API_KEY,
-// 	ENV_API_KEY_DEFAULT
-// } from '../constants'
 
 @Controller('user')
 export class UserController {
 	constructor(
-		// private readonly matchlistService: MatchlistService,
-		// private readonly configService: ConfigService,
 		@Inject(Logger)
 		private readonly logger: LoggerService
 	) { }
@@ -32,8 +22,6 @@ export class UserController {
 	@HttpCode(HttpStatus.OK)
 	@Header('Cache-Control', 'none')
 	async refreshUserData(): Promise<string> {
-		// const apiKey = this.configService.get(ENV_API_KEY, ENV_API_KEY_DEFAULT)
-
 		const dirContainingPackage = join(__dirname, '..', '..')
 		this.logger.warn(
 			`About to execute script in dir "${dirContainingPackage}" ...`,
@@ -52,7 +40,6 @@ export class UserController {
 
 		this.logger.warn(`Script completed`, ' refreshUserData | user-ctrl ')
 
-		// return this.matchlistService.getMasteryTotal(apiKey, summonerId)
 		return 'OK'
 	}
 }
