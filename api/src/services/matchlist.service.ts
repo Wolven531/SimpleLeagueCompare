c02ed134f326b62c30f75c03fa4eda6824fe97fe
Anthony Williams <anthony.williams.cs@gmail.com>
2020-09-03 18:40:19 -0600

    Remove comments

--- api/src/services/matchlist.service.ts
+++ api/src/services/matchlist.service.ts
@@ -190,31 +190,4 @@ export class MatchlistService {
 				return loadedUsers
 			})
 	}
-
-	/*
-	const fetchMatchList = async (encryptedAccountKey: string): Promise<void> => {
-	  // const headers: Headers = new Headers()
-	  // headers.set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36')
-	  // headers.set('Accept-Language', 'en-US,en;q=0.9')
-	  // headers.set('Accept-Charset', 'application/x-www-form-urlencoded; charset=UTF-8')
-	  // headers.set('X-Riot-Token', 'devAPIKey)
-  
-	  // NOTE: using token in headers appears to be broken due to pre-flight OPTIONS request in Chrome
-	  return fetch(`https://${REGION}.api.riotgames.com/lol/match/v4/matchlists/by-account/${encryptedAccountKey}?api_key=${devAPIKey}`, {
-		// cache: 'no-cache', // no-cache, reload, force-cache, only-if-cached
-		// credentials: 'same-origin', // include, same-origin, omit
-		// headers: {
-		  // 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36',
-		  // 'Accept-Language': 'en-US,en;q=0.9',
-		  // 'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
-		  // NOTE: cannot use custom header (to keep request simple enough for CORS)
-		  //   'X-Riot-Token': devAPIKey
-		// },
-		// method: 'get',
-		// mode: 'cors', // 'no-cors'
-		// redirect: 'follow', // manual, *follow, error
-		// referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-	  })
-	}
-	*/
 }
