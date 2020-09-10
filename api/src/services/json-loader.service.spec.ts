0f736c57d054e87e1a9644dea94dcd4948c184c6
Anthony Williams <anthony.williams.cs@gmail.com>
2020-09-09 22:55:39 -0600

    Update unit tests for updateUsersFile
    * They now use the standard approach

--- api/src/services/json-loader.service.spec.ts
+++ api/src/services/json-loader.service.spec.ts
@@ -26,6 +26,13 @@ type TestCase_LoadFromFile = {
 	mockReadFileSync: jest.Mock
 	name: string
 }
+type TestCase_UpdateUsersFile = {
+	expectedCountError: number
+	expectedCountLog: number
+	mockWriteFileSync: jest.Mock
+	name: string
+	param: User[]
+}
 
 describe('JSON Loader Service', () => {
 	let service: JsonLoaderService
@@ -69,63 +76,48 @@ describe('JSON Loader Service', () => {
 				.mockRestore()
 		})
 
-		describe('w/ mocked fs.writeFileSync', () => {
-			let mockWriteFileSync: jest.Mock
-
-			beforeEach(() => {
-				mockWriteFileSync = jest.fn((path, content, opts) => {})
-
-				jest.spyOn(fs, 'writeFileSync')
-					.mockImplementation(mockWriteFileSync)
-			})
-
-			afterEach(() => {
-				jest.spyOn(fs, 'writeFileSync')
-					.mockRestore()
-			})
-
-			describe('invoke updateUsersFile([]) [w/ empty array]', () => {
-				beforeEach(() => {
-					service.updateUsersFile([])
+		const testCases_UpdateUsersFile: TestCase_UpdateUsersFile[] = [
+			{
+				expectedCountError: 0,
+				expectedCountLog: 2,
+				mockWriteFileSync: jest.fn((path, content, opts) => {}),
+				name: 'w/ empty user array',
+				param: [],
+			},
+			{
+				expectedCountError: 1,
+				expectedCountLog: 1,
+				mockWriteFileSync: jest.fn((path, content, opts) => {
+					throw new Error('fake ajw err')
+				}),
+				name: 'w/ writeFileSync that throws error',
+				param: [],
+			},
+		]
+		testCases_UpdateUsersFile.forEach(({ expectedCountError, expectedCountLog, mockWriteFileSync, name, param }) => {
+			describe(`w/ mocked fs.writeFileSync (${name})`, () => {	
+				beforeEach(() => {	
+					jest.spyOn(fs, 'writeFileSync')
+						.mockImplementation(mockWriteFileSync)
 				})
 
-				it('logs file info before and after IO update', () => {
-					expect(mockWriteFileSync).toHaveBeenCalledTimes(1)
-					expect(mockLog).toHaveBeenCalledTimes(2)
-					expect(mockError).not.toHaveBeenCalled()
-				})
-			})
-		})
-
-		describe('w/ mocked fs.writeFileSync that throws Error', () => {
-			const fakeError = new Error('fake ajw err')
-			let mockWriteFileSync: jest.Mock
-
-			beforeEach(() => {
-				mockWriteFileSync = jest.fn((path, content, opts) => {
-					throw fakeError
+				afterEach(() => {
+					jest.spyOn(fs, 'writeFileSync')
+						.mockRestore()
 				})
 
-				jest.spyOn(fs, 'writeFileSync')
-					.mockImplementation(mockWriteFileSync)
-			})
+				describe(`invoke updateUsersFile(${param.length}-length user array)`, () => {
+					beforeEach(() => {
+						expect(() => {
+							service.updateUsersFile(param)
+						}).not.toThrow()
+					})
 
-			afterEach(() => {
-				jest.spyOn(fs, 'writeFileSync')
-					.mockRestore()
-			})
-
-			describe('invoke updateUsersFile([])', () => {
-				beforeEach(() => {
-					expect(() => {
-						service.updateUsersFile([])
-					}).not.toThrow()
-				})
-
-				it('logs error and invokes log once (log after update does not execute)', () => {
-					expect(mockWriteFileSync).toHaveBeenCalledTimes(1)
-					expect(mockLog).toHaveBeenCalledTimes(1)
-					expect(mockError).toHaveBeenCalledTimes(1)
+					it('invokes fs.writeFileSync, log, error correctly and does not throw Error', () => {
+						expect(mockWriteFileSync).toHaveBeenCalledTimes(1)
+						expect(mockLog).toHaveBeenCalledTimes(expectedCountLog)
+						expect(mockError).toHaveBeenCalledTimes(expectedCountError)
+					})
 				})
 			})
 		})
@@ -175,19 +167,19 @@ describe('JSON Loader Service', () => {
 					jest.spyOn(service, 'loadUsersFromFile')
 						.mockImplementation(mockLoadUsersFromFile)
 				})
-	
+
 				afterEach(() => {
 					jest.spyOn(service, 'loadUsersFromFile')
 						.mockRestore()
 				})
-	
+
 				describe(`invoke getUserByFriendlyName("${param}")`, () => {
 					let actualResult: User | undefined
-	
+
 					beforeEach(() => {
 						actualResult = service.getUserByFriendlyName(param)
 					})
-	
+
 					it('invokes loadUsersFromFile, log, error correctly and returns expected result', () => {
 						expect(mockLoadUsersFromFile).toHaveBeenCalledTimes(1)
 						expect(mockLog).toHaveBeenCalledTimes(expectedCountLog)
@@ -232,19 +224,19 @@ describe('JSON Loader Service', () => {
 					jest.spyOn(service, 'loadUsersFromFile')
 						.mockImplementation(mockLoadUsersFromFile)
 				})
-	
+
 				afterEach(() => {
 					jest.spyOn(service, 'loadUsersFromFile')
 						.mockRestore()
 				})
-	
+
 				describe('invoke isUsersFileFresh()', () => {
 					let actualResult: boolean
-	
+
 					beforeEach(() => {
 						actualResult = service.isUsersFileFresh()
 					})
-	
+
 					it('invokes loadUsersFromFile, log, error correctly and returns expected result', () => {
 						expect(mockLoadUsersFromFile).toHaveBeenCalledTimes(1)
 						expect(mockLog).toHaveBeenCalledTimes(expectedCountLog)
@@ -298,19 +290,19 @@ describe('JSON Loader Service', () => {
 					jest.spyOn(fs, 'readFileSync')
 						.mockImplementation(mockReadFileSync)
 				})
-	
+
 				afterEach(() => {
 					jest.spyOn(fs, 'readFileSync')
 						.mockRestore()
 				})
-	
+
 				describe('invoke loadUsersFromFile()', () => {
 					let actualResult: User[]
-	
+
 					beforeEach(() => {
 						actualResult = service.loadUsersFromFile()
 					})
-	
+
 					it('invokes read, log, error correctly and returns expected result', () => {
 						expect(mockReadFileSync).toHaveBeenCalledTimes(1)
 						expect(mockLog).toHaveBeenCalledTimes(expectedCountLog)
