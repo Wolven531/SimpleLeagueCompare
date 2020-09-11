472571f86abf74b590ef87d1ce150e00a260c1d2
Anthony Williams <anthony.williams.cs@gmail.com>
2020-09-10 21:35:50 -0600

    Start stats service unit tests

---
+++ api/src/services/stats.service.spec.ts
@@ -0,0 +1,47 @@
+import { HttpModule, Logger } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import { StatsService } from './stats.service'
+
+describe('Stats Service', () => {
+	let service: StatsService
+	let testModule: TestingModule
+
+	beforeEach(async () => {
+		testModule = await Test.createTestingModule({
+			controllers: [],
+			imports: [HttpModule],
+			providers: [
+				StatsService,
+				Logger,
+			],
+		}).compile()
+
+		service = testModule.get(StatsService)
+	})
+
+	afterEach(async () => {
+		await testModule.close()
+	})
+
+	describe('w/ mocked logger functions [ error, log ]', () => {
+		let mockError: jest.Mock
+		let mockLog: jest.Mock
+
+		beforeEach(() => {
+			mockError = jest.fn((msg, ...args) => {})
+			mockLog = jest.fn((msg, ...args) => {})
+
+			jest.spyOn(testModule.get(Logger), 'error')
+				.mockImplementation(mockError)
+			jest.spyOn(testModule.get(Logger), 'log')
+				.mockImplementation(mockLog)
+		})
+
+		afterEach(() => {
+			jest.spyOn(testModule.get(Logger), 'error')
+				.mockRestore()
+			jest.spyOn(testModule.get(Logger), 'log')
+				.mockRestore()
+		})
+	})
+})
