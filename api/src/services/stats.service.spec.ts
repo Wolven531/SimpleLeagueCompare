fd32a710b39f1bdf1b0203dcfbdb3b62da8c8ce3
Anthony Williams <anthony.williams.cs@gmail.com>
2020-09-13 17:24:27 -0600

    Add extra game data for unit test

--- api/src/services/stats.service.spec.ts
+++ api/src/services/stats.service.spec.ts
@@ -39,6 +39,62 @@ describe('Stats Service', () => {
 	})
 
 	describe('w/ mocked logger functions [ error, log ]', () => {
+		const generatePlayer = (id: number): Player =>
+			new Player('p', `a${id}`, `sn${id}`, `s${id}`, 'p', `a${id}`, 'm', 0)
+		const generateParticipantIdentity = (id: number): ParticipantIdentity =>
+			new ParticipantIdentity(id, generatePlayer(id))
+		const fakeGame: Game = new Game(
+			(new Date()).getTime(),
+			60 * 20,
+			1,
+			'',
+			'',
+			'',
+			0,
+			[
+				generateParticipantIdentity(1),
+				generateParticipantIdentity(2)
+			],
+			[
+				new Participant(
+					1,
+					100,
+					266, // Aatrox ?
+					7, // SummonerHeal - http://ddragon.leagueoflegends.com/cdn/10.18.1/data/en_US/summoner.json
+					4, // SummonerFlash
+					{
+						assists: 10,
+						deaths: 1,
+						goldEarned: 9000,
+						kills: 10,
+						win: true,
+					} as Stats,
+					new Timeline(1, {}, {}, {}, {}, {}, {}, {}, 'SUPPORT', 'BOTTOM'),
+				),
+				new Participant(
+					2,
+					200,
+					266, // Aatrox ?
+					4, // SummonerFlash
+					7, // SummonerHeal - http://ddragon.leagueoflegends.com/cdn/10.18.1/data/en_US/summoner.json
+					{
+						assists: 1,
+						deaths: 10,
+						goldEarned: 3000,
+						kills: 1,
+						win: false,
+					} as Stats,
+					new Timeline(2, {}, {}, {}, {}, {}, {}, {}, 'SUPPORT', 'TOP'),
+				),
+			],
+			'p',
+			0,
+			2020,
+			[
+				new Team(100, 'win', true, true, true, true, true, true, 5, 1, 1, 1, 0, 1, 0, []),
+				new Team(200, 'loss', false, false, false, false, false, false, 1, 0, 0, 0, 0, 0, 0, []),
+			]
+		)
 		let mockError: jest.Mock
 		let mockLog: jest.Mock
 
@@ -59,11 +115,6 @@ describe('Stats Service', () => {
 				.mockRestore()
 		})
 
-		const generatePlayer = (id: number): Player =>
-			new Player('p', `a${id}`, `sn${id}`, `s${id}`, 'p', `a${id}`, 'm', 0)
-		const generateParticipantIdentity = (id: number): ParticipantIdentity =>
-			new ParticipantIdentity(id, generatePlayer(id))
-
 		const testCases_CalculateGeneralStats: TestCase_CalculateGeneralStats[] = [
 			{
 				expectedResult: new CalculatedStats(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
@@ -74,43 +125,7 @@ describe('Stats Service', () => {
 			{
 				expectedResult: new CalculatedStats(1, 9000, 9000, 20, 1200, 1200, 10, 10, 1, 1, 10, 10, 0, 1, 100),
 				param1: 'a1',
-				param2: [
-					new Game(
-						(new Date()).getTime(),
-						60 * 20,
-						1,
-						'',
-						'',
-						'',
-						0,
-						[
-							generateParticipantIdentity(1),
-							generateParticipantIdentity(2)
-						],
-						[
-							new Participant(
-								1,
-								100,
-								266, // Aatrox ?
-								7, // SummonerHeal - http://ddragon.leagueoflegends.com/cdn/10.18.1/data/en_US/summoner.json
-								4, // SummonerFlash
-								{
-									assists: 10,
-									deaths: 1,
-									goldEarned: 9000,
-									kills: 10,
-									win: true,
-								} as Stats,
-								new Timeline(1, {}, {}, {}, {}, {}, {}, {}, 'SUPPORT', 'BOTTOM'),
-							),
-						],
-						'p',
-						0,
-						2020,
-						[
-							new Team(100, 'win', true, true, true, true, true, true, 5, 1, 1, 1, 0, 1, 0, [])
-						]),
-				],
+				param2: [ fakeGame ],
 				name: 'a single Game that matches',
 			},
 		]
