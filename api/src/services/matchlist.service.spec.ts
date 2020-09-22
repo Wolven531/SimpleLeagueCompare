dfd5a3f6c76e949c4009500a636afce99a87b1d1
Anthony Williams <anthony.williams.cs@gmail.com>
2020-09-21 21:10:12 -0600

    Start unit testing matchlist service

---
+++ api/src/services/matchlist.service.spec.ts
@@ -0,0 +1,127 @@
+import { Game } from '@models/game.model'
+import { HttpModule, HttpService, Logger } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import { from } from 'rxjs'
+import { MatchlistService } from './matchlist.service'
+
+type TestCase_GetGame = {
+	descriptionMockedBehavior: string
+	descriptionParams: string
+	expectedCountError: number
+	expectedCountGet: number
+	expectedCountLog: number
+	expectedResult: Game | null
+	mockHttpGet: jest.Mock
+	param1: string
+	param2: number
+}
+
+describe('Matchlist Service', () => {
+	let service: MatchlistService
+	let testModule: TestingModule
+
+	beforeEach(async () => {
+		testModule = await Test.createTestingModule({
+			controllers: [],
+			imports: [HttpModule],
+			providers: [
+				MatchlistService,
+				Logger,
+			],
+		}).compile()
+
+		service = testModule.get(MatchlistService)
+	})
+
+	afterEach(async () => {
+		await testModule.close()
+	})
+
+	describe('w/ mocked logger functions [ error, log ]', () => {
+		let mockError: jest.Mock
+		let mockLog: jest.Mock
+
+		beforeEach(() => {
+			mockError = jest.fn((msg, ...args) => {})
+			mockLog = jest.fn((msg, ...args) => {})
+
+			jest.spyOn(testModule.get(Logger), 'error')
+				.mockImplementation(mockError)
+			jest.spyOn(testModule.get(Logger), 'log')
+				.mockImplementation(mockLog)
+		})
+
+		afterEach(() => {
+			jest.spyOn(testModule.get(Logger), 'error')
+				.mockRestore()
+			jest.spyOn(testModule.get(Logger), 'log')
+				.mockRestore()
+		})
+
+		const testCases_getGame: TestCase_GetGame[] = [
+			{
+				descriptionMockedBehavior: 'Http error occurs',
+				descriptionParams: 'empty API key, gameId=0',
+				expectedCountError: 1,
+				expectedCountGet: 1,
+				expectedCountLog: 0,
+				expectedResult: null,
+				mockHttpGet: jest.fn(() => from(Promise.reject(new Error('Fake ajw error')))),
+				param1: '',
+				param2: 0,
+			},
+		]
+		testCases_getGame.forEach((
+			{
+				descriptionMockedBehavior,
+				descriptionParams,
+				expectedCountError,
+				expectedCountGet,
+				expectedCountLog,
+				expectedResult,
+				mockHttpGet,
+				param1,
+				param2,
+			}) => {
+			describe(`w/ mocked HttpGet (${descriptionMockedBehavior})`, () => {
+				beforeEach(() => {
+					jest.spyOn(testModule.get(HttpService), 'get')
+						.mockImplementation(mockHttpGet)
+				})
+
+				afterEach(() => {
+					jest.spyOn(testModule.get(HttpService), 'get')
+						.mockRestore()
+				})
+
+				describe(`invoke getGame("${param1}", "${param2}") [${descriptionParams}]`, () => {
+					let actualResult: Game | null
+
+					beforeEach(async () => {
+						actualResult = await service.getGame(param1, param2)
+					})
+
+					it('invokes get(), log(), error() correctly and returns expected result', () => {
+						expect(mockError).toHaveBeenCalledTimes(expectedCountError)
+						expect(mockLog).toHaveBeenCalledTimes(expectedCountLog)
+
+						expect(mockHttpGet).toHaveBeenCalledTimes(expectedCountGet)
+						if (expectedCountGet > 0) {
+							expect(mockHttpGet).toHaveBeenLastCalledWith(
+								`https://na1.api.riotgames.com/lol/match/v4/matches/0`,
+								{
+									headers: {
+										'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
+										'Accept-Language': 'en-US,en;q=0.9',
+										'X-Riot-Token': param1,
+									},
+								})
+						}
+
+						expect(actualResult).toEqual(expectedResult)
+					})
+				})
+			})
+		})
+	})
+})
