69ea5c5c00882bb6e29be626852eb9fe5f380f31
Anthony Williams <anthony.williams.cs@gmail.com>
2020-09-25 22:58:22 -0600

    Fix compile error after mastery refactor

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,18 +1,18 @@
-import compression from 'compression'
-import 'reflect-metadata'
 import { Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import compression from 'compression'
+import 'reflect-metadata'
 import { AppModule } from './app/app.module'
-import { JsonLoaderService } from './services/json-loader.service'
-import { MatchlistService } from './services/matchlist.service'
 import {
 	ENV_API_KEY,
 	ENV_API_KEY_DEFAULT,
 	ENV_API_PORT,
 	ENV_API_PORT_DEFAULT
 } from './constants'
+import { JsonLoaderService } from './services/json-loader.service'
+import { MasteryService } from './services/mastery.service'
 
 async function bootstrap() {
 	const app = await NestFactory.create(
@@ -23,9 +23,9 @@ async function bootstrap() {
 		},
 	)
 	const configService = app.get(ConfigService)
-	const logger = app.get(Logger);
+	const logger = app.get(Logger)
 	const jsonLoaderService = app.get(JsonLoaderService)
-	const matchlistService = app.get(MatchlistService)
+	const masteryService = app.get(MasteryService)
 	const swaggerOptions = new DocumentBuilder()
 		.setTitle('Simple League Compare API')
 		.setDescription('This API feeds a web UI for the Simple League Compare application')
@@ -44,13 +44,13 @@ async function bootstrap() {
 	logger.debug(`Loaded apiKey from env=\t${envApiKey}`, 'bootstrap | main')
 
 	if (!jsonLoaderService.isUsersFileFresh()) {
-		logger.log(`About to refresh users...`, 'bootstrap | main')
+		logger.log('About to refresh users...', 'bootstrap | main')
 	
-		const updatedUsers = await matchlistService.refreshMasteryTotalForAllUsers(envApiKey)
+		const updatedUsers = await masteryService.refreshMasteryTotalForAllUsers(envApiKey)
 	
 		logger.log(`Updated ${updatedUsers.length} users`, 'bootstrap | main')
 	} else {
-		logger.log(`Skipping user refresh since users were all fresh`, 'bootstrap | main')
+		logger.log('Skipping user refresh since users were all fresh', 'bootstrap | main')
 	}
 
 	logger.log(`Starting to listen for NestJS app on port ${port}...`, 'bootstrap | main')
