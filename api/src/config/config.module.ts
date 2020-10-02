6948952ea1be74438a6f9dde1b95d8ffde67dc0e
Anthony Williams <anthony.williams.cs@gmail.com>
2020-10-01 22:01:34 -0600

    Remove unnecessary AppService from ConfigModule

--- api/src/config/config.module.ts
+++ api/src/config/config.module.ts
@@ -1,13 +1,11 @@
 import { HttpModule, Logger, Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { AppService } from '../services/app.service'
 import { ConfigController } from './config.controller'
 
 @Module({
 	controllers: [ConfigController],
 	imports: [HttpModule],
 	providers: [
-		AppService,
 		ConfigService,
 		Logger,
 	],
