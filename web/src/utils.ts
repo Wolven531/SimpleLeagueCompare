d86736fcd2c9605b9f461f1a467028569de2274d
Anthony Williams <anthony.williams.cs@gmail.com>
2020-10-04 23:16:43 -0600

    Start HttpClient work

--- web/src/utils.ts
+++ web/src/utils.ts
@@ -1,4 +1,9 @@
-import axios from 'axios'
+import Axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import axiosCookieJarSupport from 'axios-cookiejar-support'
+import { Agent as HttpAgent, AgentOptions as HttpAgentOptions } from 'http'
+import { Agent as HttpsAgent, AgentOptions as HttpsAgentOptions } from 'https'
+import { stringify } from 'querystring'
+import { Cookie, CookieJar } from 'tough-cookie'
 import {
 	API_V,
 	KEY_CHAMPS,
@@ -6,8 +11,208 @@ import {
 	NETWORK_TIMEOUT
 } from './constants'
 
+export interface CookieMap {
+	[key: string]: string
+}
+
+export interface Headers {
+	[key: string]: string | string[]
+}
+
+export interface HttpClientResp {
+	body: Record<string, unknown> | Record<string, unknown>[] | string
+	context: HttpClientRespCtx
+	status: number
+}
+
+export interface HttpClientRespCtx {
+	cookies: CookieMap
+	headers: Headers
+}
+
+export class HttpClient {
+	/**
+	 * 
+	 * @param cookieObjectStringOrArray 
+	 */
+	static transformCookiesToString(cookieObjectStringOrArray: CookieMap | string | string[]): string {
+		// if already string, just return it
+		if (typeof cookieObjectStringOrArray === 'string') {
+			return cookieObjectStringOrArray
+		}
+		
+		// if array, convert to single string
+		if (Array.isArray(cookieObjectStringOrArray)) {
+			return cookieObjectStringOrArray.map((fullCookieString: string) => {
+				const cookie: Cookie = Cookie.parse(fullCookieString) as Cookie
+
+				return `${cookie.key}=${cookie.value};`
+			}).join('')
+		}
+	
+		// otherwise, treat as plain object w/ key/value props
+		return Object.keys(cookieObjectStringOrArray)
+			.map(key => `${key}=${cookieObjectStringOrArray[key]}`)
+			.join(';')
+	}
+
+	private static readonly DEFAULT_AGENT_OPTS: HttpAgentOptions | HttpsAgentOptions = {
+		keepAlive: true,
+		rejectUnauthorized: false,
+	}
+	private static readonly DEFAULT_REQ_CONFIG: AxiosRequestConfig = {
+		headers: {
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
+		},
+		httpAgent: new HttpAgent(HttpClient.DEFAULT_AGENT_OPTS as HttpAgentOptions),
+		httpsAgent: new HttpsAgent(HttpClient.DEFAULT_AGENT_OPTS as HttpsAgentOptions),
+		jar: new CookieJar(),
+		withCredentials: true,
+	}
+
+	/**
+	 * 
+	 * @param data 
+	 * @param headers 
+	 */
+	private static doesDataNeedStringify(data: any, headers: Headers = {}): boolean {
+		const contentTypeKey: string = Object.keys(headers).find(key => key.toLowerCase() === 'content-type') || 'content-type'
+
+		if (!headers[contentTypeKey]) {
+			headers[contentTypeKey] = 'application/x-www-form-urlencoded'
+		}
+
+		const shouldBeString: boolean = headers[contentTypeKey] === 'application/x-www-form-urlencoded'
+
+		return shouldBeString && typeof data !== 'string'
+	}
+
+	private readonly axios: AxiosInstance
+	private resp: AxiosResponse | undefined
+
+
+	constructor() {
+		axiosCookieJarSupport(Axios)
+		this.axios = Axios.create(HttpClient.DEFAULT_REQ_CONFIG)
+	}
+
+	/**
+	 * 
+	 * @param config 
+	 */
+	async get(config: AxiosRequestConfig): Promise<HttpClientResp> {
+		this.resp = await this.axios({
+			...config,
+			method: 'GET',
+		})
+
+		return {
+			body: this.resp?.data,
+			context: {
+				cookies: this.getCookieMap(),
+				headers: this.resp?.headers,
+			},
+			status: this.resp?.status,
+		}
+	}
+
+	/**
+	 * 
+	 * @param config 
+	 */
+	async post(config: AxiosRequestConfig): Promise<HttpClientResp> {
+		this.resp = await this.axios({
+			...HttpClient.doesDataNeedStringify(config.data, config.headers)
+				? stringify(config.data)
+				: config.data,
+			method: 'POST',
+		})
+
+		return {
+			body: this.resp?.data,
+			context: {
+				cookies: this.getCookieMap(),
+				headers: this.resp?.headers,
+			},
+			status: this.resp?.status,
+		}
+	}
+
+	/**
+	 * 
+	 * @param cookieInfo 
+	 */
+	setCookieHeader(cookieInfo: CookieMap | string | string[]) {
+		this.axios.defaults.headers.Cookie = HttpClient.transformCookiesToString(cookieInfo)
+	}
+
+	/**
+	 * 
+	 * @param headers 
+	 */
+	setHeaders(headers: Headers) {
+		this.axios.defaults.headers = headers
+	}
+
+	/**
+	 * 
+	 * @param proxyAgent 
+	 */
+	setProxy(proxyAgent: HttpAgent | HttpsAgent) {
+		// merge proxy-agent with default https-agent to use default agent configuration
+		const httpProxyAgent: HttpAgent | HttpsAgent = Object.assign(proxyAgent, HttpClient.DEFAULT_REQ_CONFIG.httpsAgent)
+		this.axios.defaults.httpAgent = httpProxyAgent as HttpAgent
+		this.axios.defaults.httpsAgent = httpProxyAgent as HttpsAgent
+	}
+
+	/**
+	 * 
+	 * @param callback 
+	 */
+	setResponseInterceptor(callback: (data: any) => Promise<any>) {
+		this.axios.interceptors.response.use(
+			async (resp) => {
+				await callback(resp)
+				return resp
+			},
+			async (error) => {
+				await callback(error)
+				throw error
+			}
+		)
+	}
+
+	/**
+	 * 
+	 */
+	private getCookieMap(): CookieMap {
+		const configCookiesString: string = this.resp?.config.headers?.Cookie
+		const configCookies: string[] = configCookiesString ? String(configCookiesString).split(';').map(c => c.trim()) : []
+		const headerCookies: string[] = this.resp?.headers['set-cookie'] || []
+		const nonJarCookies = configCookies?.concat(headerCookies)
+		const cookieMap: CookieMap = {}
+
+		if (this.resp?.config.jar) {
+			const jarCookies: CookieJar.Serialized = (this.resp.config.jar as CookieJar).toJSON()
+
+			jarCookies.cookies.forEach((cookiesItem: any) => {
+				cookieMap[cookiesItem.key] = cookiesItem.value
+			})
+		}
+
+		if (nonJarCookies?.length) {
+			nonJarCookies.forEach((cookie) => {
+				const [key, value] = cookie.split(';')[0].split('=')
+				cookieMap[key] = value
+			})
+		}
+
+		return cookieMap
+	}
+}
+
 export const fetchChamps = (): Promise<{}> => {
-	const axiosInstance = axios.create({
+	const axiosInstance = Axios.create({
 		baseURL: 'https://ddragon.leagueoflegends.com/',
 		headers: {},
 		timeout: NETWORK_TIMEOUT,
@@ -33,7 +238,7 @@ export const fetchChamps = (): Promise<{}> => {
 }
 
 export const fetchTriggerUserRefresh = () : Promise<any> => {
-	const axiosInstance = axios.create({
+	const axiosInstance = Axios.create({
 		baseURL: '/',
 		headers: {},
 		timeout: NETWORK_TIMEOUT,
