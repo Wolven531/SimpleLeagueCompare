31a7bd228babe96c2b7ad86a768d84128f0b0ff2
Anthony Williams <anthony.williams.cs@gmail.com>
2020-10-14 21:25:17 -0600

    Add /user endpoint to return users file

--- api/src/user/user.controller.spec.ts
+++ api/src/user/user.controller.spec.ts
@@ -1,6 +1,8 @@
+import { User } from '@models/user.model'
 import { HttpModule, Logger } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
 import childProcess from 'child_process'
+import { JsonLoaderService } from '../services/json-loader.service'
 import { UserController } from './user.controller'
 
 describe('UserController', () => {
@@ -13,6 +15,7 @@ describe('UserController', () => {
 			controllers: [UserController],
 			imports: [HttpModule],
 			providers: [
+				JsonLoaderService,
 				Logger,
 			],
 		}).compile()
@@ -48,6 +51,31 @@ describe('UserController', () => {
 			expect(mockExecFileSync).toHaveBeenCalledTimes(1)
 			expect(capturedError).toBeUndefined()
 			expect(resp).toBe('OK')
+		})
+	})
+
+	describe('invoke getUsers()', () => {
+		let capturedError: Error
+		let mockLoadUsersFromFile: jest.Mock
+		let resp: User[]
+
+		beforeEach(async () => {
+			mockLoadUsersFromFile = jest.fn(() => [])
+
+			try {
+				jest.spyOn(testModule.get(JsonLoaderService), 'loadUsersFromFile')
+					.mockImplementationOnce(mockLoadUsersFromFile)
+
+				resp = await controller.getUsers()
+			} catch (err) {
+				capturedError = err
+			}
+		})
+
+		it('invokes loadUsersFromFile(), does NOT throw error', () => {
+			expect(mockLoadUsersFromFile).toHaveBeenCalledTimes(1)
+			expect(capturedError).toBeUndefined()
+			expect(resp).toEqual([])
 		})
 	})
 })
