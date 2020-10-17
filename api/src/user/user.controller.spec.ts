2e3b5700c04091e005ff08cc4f8e6f19c38b83ba
Anthony Williams <anthony.williams.cs@gmail.com>
2020-10-16 23:08:56 -0600

    Get user ctrl tests passing

--- api/src/user/user.controller.spec.ts
+++ api/src/user/user.controller.spec.ts
@@ -1,13 +1,14 @@
 import { User } from '@models/user.model'
 import { HttpModule, Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
 import childProcess from 'child_process'
 import { JsonLoaderService } from '../services/json-loader.service'
+import { SummonerService } from '../services/summoner.service'
 import { UserController } from './user.controller'
 
 describe('UserController', () => {
 	let controller: UserController
-	let mockExecFileSync: jest.Mock
 	let testModule: TestingModule
 
 	beforeEach(async () => {
@@ -15,36 +16,41 @@ describe('UserController', () => {
 			controllers: [UserController],
 			imports: [HttpModule],
 			providers: [
+				ConfigService,
 				JsonLoaderService,
+				SummonerService,
 				Logger,
 			],
 		}).compile()
 
 		controller = testModule.get(UserController)
-
-		mockExecFileSync = jest.fn()
-
-		jest.spyOn(childProcess, 'execFileSync')
-			.mockImplementation(mockExecFileSync)
 	})
 
 	afterEach(async () => {
-		jest.spyOn(childProcess, 'execFileSync')
-			.mockRestore()
-
 		await testModule.close()
 	})
 
 	describe('invoke refreshUserData()', () => {
 		let capturedError: Error
+		let mockExecFileSync: jest.Mock
 		let resp: string
 
 		beforeEach(async () => {
+			mockExecFileSync = jest.fn()
+
 			try {
+				jest.spyOn(childProcess, 'execFileSync')
+					.mockImplementation(mockExecFileSync)
+
 				resp = await controller.refreshUserData()
 			} catch (err) {
 				capturedError = err
 			}
+		})
+
+		afterEach(() => {
+			jest.spyOn(childProcess, 'execFileSync')
+				.mockRestore()
 		})
 
 		it('invokes execFileSync(), does NOT throw error', () => {
@@ -64,12 +70,17 @@ describe('UserController', () => {
 
 			try {
 				jest.spyOn(testModule.get(JsonLoaderService), 'loadUsersFromFile')
-					.mockImplementationOnce(mockLoadUsersFromFile)
+					.mockImplementation(mockLoadUsersFromFile)
 
 				resp = await controller.getUsers()
 			} catch (err) {
 				capturedError = err
 			}
+		})
+
+		afterEach(() => {
+			jest.spyOn(testModule.get(JsonLoaderService), 'loadUsersFromFile')
+				.mockRestore()
 		})
 
 		it('invokes loadUsersFromFile(), does NOT throw error', () => {
