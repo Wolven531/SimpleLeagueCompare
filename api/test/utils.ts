595ecdcdf4aa75cf78b511a432b52c0da9fc6b4a
Anthony Williams <anthony.williams.cs@gmail.com>
2020-10-20 21:04:03 -0600

    Add test utils file

---
+++ api/test/utils.ts
@@ -0,0 +1,41 @@
+import { Logger } from '@nestjs/common'
+import { TestingModule } from '@nestjs/testing'
+
+const toggleMockedLogger = (testModule: TestingModule, enable = true): Record<string, jest.Mock> => {
+	const logger: Logger = testModule.get(Logger)
+	let mockDebug: jest.Mock
+	let mockError: jest.Mock
+	let mockLog: jest.Mock
+	let mockVerbose: jest.Mock
+
+	if (enable) {
+		mockDebug = jest.fn()
+		mockError = jest.fn()
+		mockLog = jest.fn()
+		mockVerbose = jest.fn()
+
+		jest.spyOn(logger, 'debug').mockImplementation(mockDebug)
+		jest.spyOn(logger, 'error').mockImplementation(mockError)
+		jest.spyOn(logger, 'log').mockImplementation(mockLog)
+		jest.spyOn(logger, 'verbose').mockImplementation(mockVerbose)
+	} else {
+		mockDebug = logger.debug as jest.Mock
+		mockError = logger.error as jest.Mock
+		mockLog = logger.log as jest.Mock
+		mockVerbose = logger.verbose as jest.Mock
+	
+		jest.spyOn(logger, 'debug').mockRestore()
+		jest.spyOn(logger, 'error').mockRestore()
+		jest.spyOn(logger, 'log').mockRestore()
+		jest.spyOn(logger, 'verbose').mockRestore()
+	}
+
+	return {
+		mockDebug,
+		mockError,
+		mockLog,
+		mockVerbose,
+	}
+}
+
+export { toggleMockedLogger }
