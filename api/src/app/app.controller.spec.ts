5b4598ddfc98c784cf7e9e080a9d8f6aad1fc920
Anthony Williams <anthony.williams.cs@gmail.com>
2020-10-21 23:58:03 -0600

    Common mocked logger in app ctrl unit test

--- api/src/app/app.controller.spec.ts
+++ api/src/app/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { HttpModule, Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
+import { toggleMockedLogger } from '../../test/utils'
 import { AppService } from '../services/app.service'
 import { AppController } from './app.controller'
 
@@ -11,53 +12,6 @@ type TestCase_IsTokenValid = {
 }
 
 describe('AppController', () => {
-	let controller: AppController
-	let mockError: jest.Mock
-	let mockLog: jest.Mock
-	let mockVerbose: jest.Mock
-	let testModule: TestingModule
-
-	beforeEach(async () => {
-		testModule = await Test.createTestingModule({
-			controllers: [AppController],
-			imports: [HttpModule],
-			providers: [
-				{
-					provide: ConfigService,
-					useFactory: () => ({
-						get: jest.fn().mockReturnValueOnce('some-api-key'),
-					}),
-				},
-				Logger,
-				AppService,
-			],
-		}).compile()
-
-		mockError = jest.fn((msg, ...args) => {})
-		mockLog = jest.fn((msg, ...args) => {})
-		mockVerbose = jest.fn((msg, ...args) => {})
-
-		jest.spyOn(testModule.get(Logger), 'error')
-			.mockImplementation(mockError)
-		jest.spyOn(testModule.get(Logger), 'log')
-			.mockImplementation(mockLog)
-		jest.spyOn(testModule.get(Logger), 'verbose')
-			.mockImplementation(mockVerbose)
-
-		controller = testModule.get(AppController)
-	})
-
-	afterEach(async () => {
-		jest.spyOn(testModule.get(Logger), 'error')
-			.mockRestore()
-		jest.spyOn(testModule.get(Logger), 'log')
-			.mockRestore()
-		jest.spyOn(testModule.get(Logger), 'verbose')
-			.mockRestore()
-
-		await testModule.close()
-	})
-
 	const testCases: TestCase_IsTokenValid[] = [
 		{
 			descriptionMockedBehavior: 'service method returns true (mocked)',
@@ -70,31 +24,68 @@ describe('AppController', () => {
 			mockIsRiotTokenValid: jest.fn(() => Promise.resolve(false)),
 		},
 	]
-	testCases.forEach((
-		{
-			descriptionMockedBehavior,
-			expectedResult,
-			mockIsRiotTokenValid,
-		}
-	) => {
-		describe(`invoke isTokenValid() when ${descriptionMockedBehavior}`, () => {
-			let actualResult: boolean
+	let controller: AppController
+	let testModule: TestingModule
 
-			beforeEach(async () => {
-				jest.spyOn(testModule.get(AppService), 'isRiotTokenValid')
-					.mockImplementation(mockIsRiotTokenValid)
+	beforeEach(async () => {
+		testModule = await Test.createTestingModule({
+			controllers: [AppController],
+			imports: [HttpModule],
+			providers: [
+				// {
+				// 	provide: ConfigService,
+				// 	useFactory: () => ({
+				// 		get: jest.fn().mockReturnValueOnce('some-api-key'),
+				// 	}),
+				// },
+				ConfigService,
+				AppService,
+				Logger,
+			],
+		}).compile()
 
-				actualResult = await controller.isTokenValid()
-			})
+		controller = testModule.get(AppController)
+	})
 
-			afterEach(() => {
-				jest.spyOn(testModule.get(AppService), 'isRiotTokenValid')
-					.mockRestore()
-			})
+	afterEach(async () => {
+		await testModule.close()
+	})
 
-			it('invokes isRiotTokenValid() and returns expected value', () => {
-				expect(mockIsRiotTokenValid).toHaveBeenCalledTimes(1)
-				expect(actualResult).toBe(expectedResult)
+	describe('w/ mocked logger functions [ debug, error, log, verbose ]', () => {
+		beforeEach(() => {
+			toggleMockedLogger(testModule)
+		})
+
+		afterEach(() => {
+			toggleMockedLogger(testModule, false)
+		})
+
+		testCases.forEach((
+			{
+				descriptionMockedBehavior,
+				expectedResult,
+				mockIsRiotTokenValid,
+			}
+		) => {
+			describe(`invoke isTokenValid() when ${descriptionMockedBehavior}`, () => {
+				let actualResult: boolean
+	
+				beforeEach(async () => {
+					jest.spyOn(testModule.get(AppService), 'isRiotTokenValid')
+						.mockImplementation(mockIsRiotTokenValid)
+	
+					actualResult = await controller.isTokenValid()
+				})
+	
+				afterEach(() => {
+					jest.spyOn(testModule.get(AppService), 'isRiotTokenValid')
+						.mockRestore()
+				})
+	
+				it('invokes isRiotTokenValid() and returns expected value', () => {
+					expect(mockIsRiotTokenValid).toHaveBeenCalledTimes(1)
+					expect(actualResult).toBe(expectedResult)
+				})
 			})
 		})
 	})
