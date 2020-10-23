1bd76d7112b7cfe048b07d5faafa852ee2f77a22
Anthony Williams <anthony.williams.cs@gmail.com>
2020-10-22 20:50:22 -0600

    Use mocked logger in ctrl unit tests

--- api/src/mastery/mastery.controller.spec.ts
+++ api/src/mastery/mastery.controller.spec.ts
@@ -1,14 +1,13 @@
 import { HttpModule, Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
+import { toggleMockedLogger } from '../../test/utils'
 import { JsonLoaderService } from '../services/json-loader.service'
 import { MasteryService } from '../services/mastery.service'
 import { MasteryController } from './mastery.controller'
 
 describe('MasteryController', () => {
 	let controller: MasteryController
-	let mockError: jest.Mock
-	let mockLog: jest.Mock
 	let testModule: TestingModule
 
 	beforeEach(async () => {
@@ -23,34 +22,32 @@ describe('MasteryController', () => {
 			],
 		}).compile()
 
-		mockError = jest.fn((msg, ...args) => {})
-		mockLog = jest.fn((msg, ...args) => {})
-
-		jest.spyOn(testModule.get(Logger), 'error')
-			.mockImplementation(mockError)
-		jest.spyOn(testModule.get(Logger), 'log')
-			.mockImplementation(mockLog)
-
 		controller = testModule.get(MasteryController)
 	})
 
 	afterEach(async () => {
-		jest.spyOn(testModule.get(Logger), 'error')
-			.mockRestore()
-		jest.spyOn(testModule.get(Logger), 'log')
-			.mockRestore()
 		await testModule.close()
 	})
 
-	describe('invoke getMasteryTotal("") (w/ empty string)', () => {
-		let resp: number
-
-		beforeEach(async () => {
-			resp = await controller.getMasteryTotal('')
+	describe('w/ mocked logger functions [ debug, error, log, verbose ]', () => {
+		beforeEach(() => {
+			toggleMockedLogger(testModule)
 		})
 
-		it('returns -1', () => {
-			expect(resp).toBe(-1)
+		afterEach(() => {
+			toggleMockedLogger(testModule, false)
+		})
+
+		describe('invoke getMasteryTotal("") (w/ empty string)', () => {
+			let resp: number
+	
+			beforeEach(async () => {
+				resp = await controller.getMasteryTotal('')
+			})
+	
+			it('returns -1', () => {
+				expect(resp).toBe(-1)
+			})
 		})
 	})
 })
