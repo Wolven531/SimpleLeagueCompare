3123c102c53adbb543c6297656fe761dd8949340
Anthony Williams <anthony.williams.cs@gmail.com>
2020-10-23 21:59:51 -0600

    Use toggleMockedLogger - stats svc spec

--- api/src/services/stats.service.spec.ts
+++ api/src/services/stats.service.spec.ts
@@ -1,13 +1,12 @@
 import { CalculatedStats } from '@models/calculated-stats.model'
 import { Game } from '@models/game.model'
-import { ParticipantIdentity } from '@models/participant-identity.model'
 import { Participant } from '@models/participant.model'
-import { Player } from '@models/player.model'
 import { Stats } from '@models/stats.model'
 import { Team } from '@models/team.model'
 import { Timeline } from '@models/timeline.model'
 import { HttpModule, Logger } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
+import { generateParticipantIdentity, toggleMockedLogger } from '../../test/utils'
 import { StatsService } from './stats.service'
 
 type TestCase_CalculateGeneralStats = {
@@ -18,6 +17,95 @@ type TestCase_CalculateGeneralStats = {
 }
 
 describe('Stats Service', () => {
+	const fakeGame = new Game(
+		(new Date()).getTime(),
+		60 * 20,
+		1,
+		'',
+		'',
+		'',
+		0,
+		[
+			generateParticipantIdentity(1),
+			generateParticipantIdentity(2),
+		],
+		[
+			new Participant(
+				1,
+				100,
+				266, // Aatrox ?
+				7, // SummonerHeal - http://ddragon.leagueoflegends.com/cdn/10.18.1/data/en_US/summoner.json
+				4, // SummonerFlash
+				{
+					assists: 10,
+					deaths: 1,
+					goldEarned: 9000,
+					kills: 10,
+					win: true,
+				} as Stats,
+				new Timeline(1, {}, {}, {}, {}, {}, {}, {}, 'SUPPORT', 'BOTTOM'),
+			),
+			new Participant(
+				2,
+				200,
+				266, // Aatrox ?
+				4, // SummonerFlash
+				7, // SummonerHeal - http://ddragon.leagueoflegends.com/cdn/10.18.1/data/en_US/summoner.json
+				{
+					assists: 1,
+					deaths: 10,
+					goldEarned: 3000,
+					kills: 1,
+					win: false,
+				} as Stats,
+				new Timeline(2, {}, {}, {}, {}, {}, {}, {}, 'SUPPORT', 'TOP'),
+			),
+		],
+		'p',
+		0,
+		2020,
+		[
+			new Team(100, 'win', true, true, true, true, true, true, 5, 1, 1, 1, 0, 1, 0, []),
+			new Team(200, 'loss', false, false, false, false, false, false, 1, 0, 0, 0, 0, 0, 0, []),
+		]
+	)
+	const testCases_CalculateGeneralStats: TestCase_CalculateGeneralStats[] = [
+		{
+			expectedResult: new CalculatedStats(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+			name: 'empty account id and empty games array',
+			param1: '',
+			param2: [],
+		},
+		{
+			expectedResult: new CalculatedStats(1, 9000, 9000, 20, 1200, 1200, 10, 10, 1, 1, 10, 10, 0, 1, 100),
+			name: 'a single Game (w/ a win that matches)',
+			param1: 'a1',
+			param2: [ fakeGame ],
+		},
+		{
+			expectedResult: new CalculatedStats(1, 3000, 3000, 0.2, 1200, 1200, 1, 1, 10, 10, 1, 1, 1, 0, 0),
+			name: 'a single Game (w/ a loss that matches)',
+			param1: 'a2',
+			param2: [ fakeGame ],
+		},
+		{
+			expectedResult: new CalculatedStats(1, 0, 0, 0, 1200, 1200, 0, 0, 0, 0, 0, 0, 1, 0, 0),
+			name: 'a single Game (w/ no identity matches)',
+			param1: 'a3',
+			param2: [ fakeGame ],
+		},
+		{
+			expectedResult: new CalculatedStats(1, 0, 0, 0, 1200, 1200, 0, 0, 0, 0, 0, 0, 1, 0, 0),
+			name: 'a single Game (w/ no participant matches)',
+			param1: 'a3',
+			param2: [
+				{
+					...fakeGame,
+					participantIdentities: fakeGame.participantIdentities.concat(generateParticipantIdentity(3)),
+				},
+			],
+		},
+	]
 	let service: StatsService
 	let testModule: TestingModule
 
@@ -38,120 +126,15 @@ describe('Stats Service', () => {
 		await testModule.close()
 	})
 
-	describe('w/ mocked logger functions [ error, log ]', () => {
-		const generatePlayer = (id: number): Player =>
-			new Player('p', `a${id}`, `sn${id}`, `s${id}`, 'p', `a${id}`, 'm', 0)
-		const generateParticipantIdentity = (id: number): ParticipantIdentity =>
-			new ParticipantIdentity(id, generatePlayer(id))
-		const fakeGame: Game = new Game(
-			(new Date()).getTime(),
-			60 * 20,
-			1,
-			'',
-			'',
-			'',
-			0,
-			[
-				generateParticipantIdentity(1),
-				generateParticipantIdentity(2),
-			],
-			[
-				new Participant(
-					1,
-					100,
-					266, // Aatrox ?
-					7, // SummonerHeal - http://ddragon.leagueoflegends.com/cdn/10.18.1/data/en_US/summoner.json
-					4, // SummonerFlash
-					{
-						assists: 10,
-						deaths: 1,
-						goldEarned: 9000,
-						kills: 10,
-						win: true,
-					} as Stats,
-					new Timeline(1, {}, {}, {}, {}, {}, {}, {}, 'SUPPORT', 'BOTTOM'),
-				),
-				new Participant(
-					2,
-					200,
-					266, // Aatrox ?
-					4, // SummonerFlash
-					7, // SummonerHeal - http://ddragon.leagueoflegends.com/cdn/10.18.1/data/en_US/summoner.json
-					{
-						assists: 1,
-						deaths: 10,
-						goldEarned: 3000,
-						kills: 1,
-						win: false,
-					} as Stats,
-					new Timeline(2, {}, {}, {}, {}, {}, {}, {}, 'SUPPORT', 'TOP'),
-				),
-			],
-			'p',
-			0,
-			2020,
-			[
-				new Team(100, 'win', true, true, true, true, true, true, 5, 1, 1, 1, 0, 1, 0, []),
-				new Team(200, 'loss', false, false, false, false, false, false, 1, 0, 0, 0, 0, 0, 0, []),
-			]
-		)
-		let mockError: jest.Mock
-		let mockLog: jest.Mock
-
+	describe('w/ mocked logger functions [ debug, error, log, verbose ]', () => {
 		beforeEach(() => {
-			mockError = jest.fn((msg, ...args) => {})
-			mockLog = jest.fn((msg, ...args) => {})
-
-			jest.spyOn(testModule.get(Logger), 'error')
-				.mockImplementation(mockError)
-			jest.spyOn(testModule.get(Logger), 'log')
-				.mockImplementation(mockLog)
+			toggleMockedLogger(testModule)
 		})
 
 		afterEach(() => {
-			jest.spyOn(testModule.get(Logger), 'error')
-				.mockRestore()
-			jest.spyOn(testModule.get(Logger), 'log')
-				.mockRestore()
+			toggleMockedLogger(testModule, false)
 		})
 
-		const testCases_CalculateGeneralStats: TestCase_CalculateGeneralStats[] = [
-			{
-				expectedResult: new CalculatedStats(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-				name: 'empty account id and empty games array',
-				param1: '',
-				param2: [],
-			},
-			{
-				expectedResult: new CalculatedStats(1, 9000, 9000, 20, 1200, 1200, 10, 10, 1, 1, 10, 10, 0, 1, 100),
-				name: 'a single Game (w/ a win that matches)',
-				param1: 'a1',
-				param2: [ fakeGame ],
-			},
-			{
-				expectedResult: new CalculatedStats(1, 3000, 3000, 0.2, 1200, 1200, 1, 1, 10, 10, 1, 1, 1, 0, 0),
-				name: 'a single Game (w/ a loss that matches)',
-				param1: 'a2',
-				param2: [ fakeGame ],
-			},
-			{
-				expectedResult: new CalculatedStats(1, 0, 0, 0, 1200, 1200, 0, 0, 0, 0, 0, 0, 1, 0, 0),
-				name: 'a single Game (w/ no identity matches)',
-				param1: 'a3',
-				param2: [ fakeGame ],
-			},
-			{
-				expectedResult: new CalculatedStats(1, 0, 0, 0, 1200, 1200, 0, 0, 0, 0, 0, 0, 1, 0, 0),
-				name: 'a single Game (w/ no participant matches)',
-				param1: 'a3',
-				param2: [
-					{
-						...fakeGame,
-						participantIdentities: fakeGame.participantIdentities.concat(generateParticipantIdentity(3)),
-					},
-				],
-			},
-		]
 		testCases_CalculateGeneralStats.forEach(({ expectedResult, param1, param2, name }) => {
 			describe(`invoke calculateGeneralStats("${param1}", ${param2.length} games) [${name}]`, () => {
 				let actualResult: CalculatedStats
@@ -162,8 +145,6 @@ describe('Stats Service', () => {
 
 				it('returns expected CalculatedStats', () => {
 					expect(actualResult).toEqual(expectedResult)
-					expect(mockLog).toHaveBeenCalledTimes(1)
-					expect(mockError).not.toHaveBeenCalled()
 				})
 			})
 		})
