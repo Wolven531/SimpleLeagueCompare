4ee75503259983f58aa0d3d001bfdd5771c7b77a
Anthony Williams <anthony.williams.cs@gmail.com>
2020-10-24 20:12:49 -0600

    Add FuncUserInfoFetch to common-types

--- web/src/common-types.ts
+++ web/src/common-types.ts
@@ -1,8 +1,11 @@
 import { CalculatedStats } from './models/calculated-stats.model'
 
 export type FuncMasteryFetch = (apiUrl: string, summonerId: string, defaultTotalMastery?: number) => Promise<number>
+
 export type FuncStatsFetch = (
 	apiUrl: string,
 	summonerId: string,
 	defaultCalculatedStats?: CalculatedStats,
 	numToFetch?: number) => Promise<CalculatedStats>
+
+export type FuncUserInfoFetch = (apiUrl: string) => Promise<Record<string, unknown>>
