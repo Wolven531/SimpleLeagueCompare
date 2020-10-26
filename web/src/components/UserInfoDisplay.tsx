8ec68ad96be639c26bfa717296c54032115b7eb2
Anthony Williams <anthony.williams.cs@gmail.com>
2020-10-25 21:25:40 -0600

    Display user file w/ basic style

--- web/src/components/UserInfoDisplay.tsx
+++ web/src/components/UserInfoDisplay.tsx
@@ -8,13 +8,17 @@ export interface IUserInfoDisplay {
 
 const TOKEN_COMP = 'UserInfoDisplay'
 
-const defaultFetchUserInfo: FuncUserInfoFetch = (apiUrl: string): Promise<any> => {
-	const TOKEN_FUNC = `[ fetchUserInfoDefault | ${TOKEN_COMP} ]`
+const defaultFetchUserInfo: FuncUserInfoFetch = (apiUrl: string): Promise<Record<string, unknown>> => {
+	const TOKEN_FUNC = `[ defaultFetchUserInfo | ${TOKEN_COMP} ]`
 	const url = `${apiUrl}/user`
+
+	console.info(`${TOKEN_FUNC} About to fetch user info at "${url}"`)
 
 	return fetch(url)
 		.then(response => response.json())
 		.then((userInfo: Record<string, unknown>) => {
+			console.log(`${TOKEN_FUNC} Successfully got JSON and parsed to object`)
+
 			return userInfo
 		})
 		.catch(err => {
@@ -34,10 +38,28 @@ const UserInfoDisplay: FC<IUserInfoDisplay> = ({
 		fetchUserInfo(apiUrl).then(setUserInfo)
 	}, [apiUrl, fetchUserInfo])
 
+	const numExtraLinesToShow = 1
+	const numLinesPerUser = 7
+	const numUsersToShow = 2
+	const textareaRows = numUsersToShow * numLinesPerUser + numExtraLinesToShow
+	const textareaDisplayVal = JSON.stringify(userInfo, null, '\t')
+
 	return (
 		<div className="user-info-container">
-			<p>
-			</p>
+			<h3>Current Users file</h3>
+			<textarea
+				cols={81}
+				readOnly
+				rows={textareaRows}
+				style={{
+					backgroundColor: '#333',
+					color: '#ccc',
+					padding: 10,
+				}}
+				value={textareaDisplayVal}
+				// more info - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/textarea#attr-wrap
+				wrap="off"
+				/>
 		</div>
 	)
 }
