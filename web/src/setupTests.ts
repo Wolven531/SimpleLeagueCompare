5215a97e6435352ec6dbb84582fedcf87e0c88a2
Anthony Williams <anthony.williams.cs@gmail.com>
2020-10-27 23:13:09 -0600

    Move console mocking for unit tests to setupTests

--- web/src/setupTests.ts
+++ web/src/setupTests.ts
@@ -2,4 +2,28 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect'
+
+beforeAll(() => {
+	jest.spyOn(console, 'error')
+		.mockImplementation(jest.fn())
+	jest.spyOn(console, 'info')
+		.mockImplementation(jest.fn())
+	jest.spyOn(console, 'log')
+		.mockImplementation(jest.fn())
+
+	// jest.spyOn(NetClient, 'get')
+	// 	.mockImplementation(mockGet)
+})
+
+afterAll(() => {
+	jest.spyOn(console, 'error')
+		.mockRestore()
+	jest.spyOn(console, 'info')
+		.mockRestore()
+	jest.spyOn(console, 'log')
+		.mockRestore()
+
+	// jest.spyOn(NetClient, 'get')
+	// 	.mockRestore()
+})
