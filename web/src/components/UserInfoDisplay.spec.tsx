cc99465779bbd28f63189cc86add83c8966706c4
Anthony Williams <anthony.williams.cs@gmail.com>
2020-10-27 23:21:10 -0600

    Convert to loop unit test structure

--- web/src/components/UserInfoDisplay.spec.tsx
+++ web/src/components/UserInfoDisplay.spec.tsx
@@ -3,32 +3,46 @@ import React from 'react'
 import { HttpClientResp, NetClient } from '../utils'
 import { UserInfoDisplay } from './UserInfoDisplay'
 
+type TestCase_UserInfoDisplay = {
+	mockGet: jest.Mock
+	testDescription: string
+}
+
 describe('UserInfoDisplay', () => {
-	let component: RenderResult
+	const testCases: TestCase_UserInfoDisplay[] = [
+		{
+			mockGet: jest.fn(() => Promise.resolve({
+				body: {},
+				status: 200,
+			} as HttpClientResp)),
+			testDescription: 'when GET returns 200, body is empty object',
+		},
+	]
 
-	beforeEach(() => {
-		const mockGet = jest.fn(() => Promise.resolve({
-			body: {},
-			status: 200,
-		} as HttpClientResp))
+	testCases.forEach(({ mockGet, testDescription }) => {
+		let component: RenderResult
 
-		jest.spyOn(NetClient, 'get')
-			.mockImplementation(mockGet)
+		describe(`${testDescription}`, () => {
+			beforeEach(() => {
+				jest.spyOn(NetClient, 'get')
+					.mockImplementation(mockGet)
 
-		component = render(<UserInfoDisplay apiUrl="http://some-api.com" />)
-	})
+				component = render(<UserInfoDisplay apiUrl="http://some-api.com" />)
+			})
 
-	afterEach(() => {
-		jest.spyOn(NetClient, 'get')
-			.mockRestore()
-	})
+			afterEach(() => {
+				jest.spyOn(NetClient, 'get')
+					.mockRestore()
+			})
 
-	it('renders container, header, and refresh button', () => {
-		const elems = component.baseElement.getElementsByClassName('user-info-container')
-		expect(elems).toHaveLength(1)
-		expect(elems[0]).toBeInTheDocument()
-
-		expect(component.getByText(/Current Users file/g)).toBeInTheDocument()
-		expect(component.getByText(/Refresh Info/g)).toBeInTheDocument()
+			it('renders container, header, and refresh button', () => {
+				const elems = component.baseElement.getElementsByClassName('user-info-container')
+				expect(elems).toHaveLength(1)
+				expect(elems[0]).toBeInTheDocument()
+		
+				expect(component.getByText(/Current Users file/g)).toBeInTheDocument()
+				expect(component.getByText(/Refresh Info/g)).toBeInTheDocument()
+			})
+		})
 	})
 })
